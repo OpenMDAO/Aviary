a7c18fc898e31d0db73f5b9d139d9434675c235a
Carl Recine <carl.recine@nasa.gov>
2023-12-04 16:45:52 -0800

    Converted takeoff and landing ODEs to be compatible with SGM

--- aviary/mission/flops_based/ode/takeoff_ode.py
+++ aviary/mission/flops_based/ode/takeoff_ode.py
@@ -7,12 +7,14 @@ from dymos.models.atmosphere.atmos_1976 import USatm1976Comp
 
 from aviary.mission.flops_based.ode.takeoff_eom import StallSpeed, TakeoffEOM
 from aviary.mission.gasp_based.flight_conditions import FlightConditions
+from aviary.mission.gasp_based.ode.time_integration_base_classes import add_SGM_required_inputs
 from aviary.utils.aviary_values import AviaryValues
 from aviary.utils.functions import set_aviary_initial_values, promote_aircraft_and_mission_vars
 from aviary.variable_info.variables import Aircraft
 from aviary.variable_info.variables import Dynamic as _Dynamic
 from aviary.variable_info.variables import Mission
 from aviary.variable_info.variables_in import VariablesIn
+from aviary.variable_info.enums import AnalysisScheme
 
 Dynamic = _Dynamic.Mission
 
@@ -62,13 +64,28 @@ class TakeoffODE(om.Group):
             'climbing', default=False, types=bool,
             desc='mode of operation (ground roll or flight)')
 
+        self.options.declare(
+            "analysis_scheme",
+            default=AnalysisScheme.COLLOCATION,
+            types=AnalysisScheme,
+            desc="The analysis method that will be used to close the trajectory; for example collocation or time integration",
+        )
+
     def setup(self):
         options = self.options
 
         nn = options["num_nodes"]
+        analysis_scheme = options['analysis_scheme']
         aviary_options = options['aviary_options']
         subsystem_options = options['subsystem_options']
         core_subsystems = options['core_subsystems']
+
+        if analysis_scheme is AnalysisScheme.SHOOTING:
+            SGM_required_inputs = {
+                't_curr': {'units': 's'},
+                Dynamic.RANGE: {'units': 'm'},
+            }
+            add_SGM_required_inputs(self, SGM_required_inputs)
 
         self.add_subsystem(
             'input_port', VariablesIn(aviary_options=aviary_options),
