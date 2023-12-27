8d0bf401b971f55c8c0e3be0c4fc7ed609cb622f
Carl Recine <carl.recine@nasa.gov>
2023-12-26 17:03:45 -0800

    Fixed issues with conda environments not building successfully

--- .github/install_env_from_github.sh
+++ .github/install_env_from_github.sh
@@ -21,7 +21,8 @@ output_yaml="modified_environment.yml"
 if [ "$#" -ge 2 ]; then
     env_name=$2
 else
-    env_name="debug_env"
+    read -r firstline<$input_yaml
+    env_name=${firstline#*name: }
 fi
 
 # Determine if mamba is available and use it; otherwise, use conda
@@ -45,10 +46,10 @@ else
 fi
 
 # Extract pyoptsparse line
-pyoptsparse_line=$(grep ' pyoptsparse' $input_yaml | sed 's/^    //')
+# pyoptsparse_line=$(grep ' pyoptsparse' $input_yaml | sed 's/^    //')
 
 # Remove specified packages and write to an intermediate file
-grep -v -e 'aviary' -e 'build-pyoptsparse' -e 'pyoptsparse' $input_yaml > $intermediate_yaml
+grep -v -e 'aviary' -e 'om-aviary' -e 'build-pyoptsparse' -e 'pyoptsparse' -e 'boring-battery' -e 'networkx' -e '  - pip:' $input_yaml > $intermediate_yaml
 
 # Check for 'dev' versions of OpenMDAO and Dymos
 if grep -q -e 'openmdao.*dev' $intermediate_yaml; then
@@ -60,25 +61,45 @@ fi
 if grep -q -e 'dymos.*dev' $intermediate_yaml; then
     install_dymos_from_git=true
     grep -v -e 'dymos' $intermediate_yaml > $output_yaml
-else
-    mv $intermediate_yaml $output_yaml
+    mv $output_yaml $intermediate_yaml  # Move output to intermediate for further processing
 fi
 
-# Insert pyoptsparse line after the 'dependencies' line
-awk -v p="$pyoptsparse_line" '/dependencies/ {print; print p; next}1' $output_yaml > tmp && mv tmp $output_yaml
+# Move remaining pip installs to a separate requirements.txt file
+# Prevents conda evironment from failing to build if there is an issue with the pip installs
+pip_prefix='      - '
+requirements_txt=$env_name"_requirements.txt"
+rm -f $requirements_txt
+while IFS= read -r line
+do
+    case $line in *$pip_prefix* )
+        package=${line#*$pip_prefix}
+        echo $package >> $requirements_txt
+        ;;
+        *)
+        echo "$line" >> $output_yaml
+    esac
+done < "$intermediate_yaml"
 
-# Remove the intermediate file
-rm -f $intermediate_yaml
+# Insert pyoptsparse line after the 'dependencies' line
+# awk -v p="$pyoptsparse_line" '/dependencies/ {print; print p; next}1' $output_yaml > tmp && mv tmp $output_yaml
 
 # Create a new conda environment from the modified YAML file
 $pkg_manager env create -f $output_yaml -n $env_name
 
-source activate base
+# Attempting to resolve "CommandNotFoundError: Your shell has not been properly configured to use 'conda activate'."
+# tmp=${SHELL##*/}
+# MYSHELL=${tmp##*\\}
+# $pkg_manager init $MYSHELL
+# source activate base
 
 # Activate the new conda environment
 conda activate $env_name
 
 # Check flags and install special packages if needed
+pip install -r $requirements_txt
+
+pip install "boring_battery @ git+https://github.com/jcchin/boring.git"
+
 if [ "$install_openmdao_from_git" = true ]; then
     pip install git+https://github.com/OpenMDAO/OpenMDAO.git
 fi
@@ -86,6 +107,14 @@ fi
 if [ "$install_dymos_from_git" = true ]; then
     pip install git+https://github.com/OpenMDAO/dymos.git
 fi
+
+# Clean up
+# Remove the intermediate file
+rm -f $intermediate_yaml
+# Remove the final file
+rm -f $output_yaml
+# Remove the pip requirements
+rm -f $requirements_txt
 
 # Print a reminder to install other packages manually
 echo "---------------"
