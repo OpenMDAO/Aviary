224efdffc32479e335e16c48962472e1e4de78bf
John Jasa <johnjasa11@gmail.com>
2024-01-09 15:46:15 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
             "visualization/assets/*",
             "visualization/assets/aviary_vars/*"
         ],
-        'aviary.docs': ['*.py', 
+        'aviary.docs': ['*.py',
                         'tests/*.py',
                         '*/*.md',
                         '*/*.ipynb',
