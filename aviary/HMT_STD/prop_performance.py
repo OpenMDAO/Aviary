a800280d2f967fd2f889fb5b374af16966b61459
Xun Jiang <xun.j.jiang@nasa.gov>
2024-02-27 08:55:08 -0800

    work in progress

--- aviary/HMT_STD/prop_performance.py
+++ aviary/HMT_STD/prop_performance.py
@@ -7,7 +7,7 @@ from aviary.variable_info.options import get_option_defaults
 from aviary.HMT_STD.hamilton_standard import HamiltonStandard, PostHamiltonStandard, PreHamiltonStandard
 import pdb
 
-def print_report1(nCase, p, nBlades):
+def print_report1(nCase, p, nBlades, nflag):
     print()
     print(f"Case {nCase}")
     diam = p.get_val(Aircraft.Engine.PROPELLER_DIAMETER)
@@ -20,18 +20,22 @@ def print_report1(nCase, p, nBlades):
     if pcli.ndim == 1:
         pcli = pcli[0]
     print(f"DPROP,BLADT,AFT,CLI: {diam}, {nBlades}, {actF}, {pcli}")
-    DiamNac = p.get_val(Aircraft.Nacelle.AVG_DIAMETER)
-    DiamProp = p.get_val(Aircraft.Engine.PROPELLER_DIAMETER)
-    if DiamNac.ndim == 0:
-        DiamNac = float(DiamNac)
+    if nflag:
+        DiamNac = p.get_val(Aircraft.Nacelle.AVG_DIAMETER)
+        if DiamNac.ndim == 0:
+            DiamNac = float(DiamNac)
+        else:
+            DiamNac = float(DiamNac[0])
     else:
-        DiamNac = float(DiamNac[0])
+        DiamNac = 0.0
+    DiamProp = p.get_val(Aircraft.Engine.PROPELLER_DIAMETER)
     if DiamProp.ndim == 0:
         DiamProp = float(DiamProp)
     else:
         DiamProp = float(DiamProp[0])
-    DiamNac_DiamProp = DiamNac/DiamProp
-    print(f"Nacalle Diam to Prop Diam Ratio: {round(DiamNac_DiamProp, 3)}")
+    if nflag:
+        DiamNac_DiamProp = DiamNac/DiamProp
+        print(f"Nacalle Diam to Prop Diam Ratio: {round(DiamNac_DiamProp, 3)}")
     alt = p.get_val(Dynamic.Mission.ALTITUDE)
     if alt.ndim == 1:
         alt = alt[0]
@@ -78,12 +82,14 @@ class PropPerf(om.Group):
             desc='collection of Aircraft/Mission specific options')
 
     def setup(self):
-            options = self.options
-            #nn = options['num_nodes']
-            options = options['aviary_options']
-            compute_installation_loss = options.get_val(Aircraft.Design.COMPUTE_INSTALLATION_LOSS)
-            num_blades = options.get_val(Aircraft.Engine.NUM_BLADES)
+        options = self.options
+        #nn = options['num_nodes']
+        options = options['aviary_options']
+        compute_installation_loss = options.get_val(Aircraft.Design.COMPUTE_INSTALLATION_LOSS)
+        num_blades = options.get_val(Aircraft.Engine.NUM_BLADES)
 
+        # JK NOTE it looks like tloss can be its own component. Optionally loaded? Or install_loss_factor is an override value? Might need to talk with Ken
+        if compute_installation_loss:
             self.add_subsystem(
                 name='sqa_comp',
                 subsys=om.ExecComp(
@@ -110,130 +116,128 @@ class PropPerf(om.Group):
                 promotes_outputs=["equiv_adv_ratio"],
             )
 
-            # JK NOTE it looks like tloss can be its own component. Optionally loaded? Or install_loss_factor is an override value? Might need to talk with Ken
-            if compute_installation_loss:
-                self.blockage_factor_interp = self.add_subsystem(
-                    "blockage_factor_interp",
-                    om.MetaModelStructuredComp(method="scipy_slinear", extrapolate=True),
-                    promotes_inputs=["sqa","equiv_adv_ratio"],
-                    promotes_outputs=[
-                        "blockage_factor",
-                    ],
-                )
-
-                self.blockage_factor_interp.add_input(
-                    "sqa",
-                    0.0,
-                    training_data=[0.00, 0.04, 0.08, 0.12, 0.16, 0.20, 0.24, 0.28, 0.32],
-                    units="unitless",
-                    desc="square of DiamNac/DiamProp",
-                )
-
-                self.blockage_factor_interp.add_input(
-                    "equiv_adv_ratio",
-                    0.0,
-                    training_data=[0., 0.5, 1.0, 2.0, 3.0, 4.0, 5.0],
-                    units="unitless",
-                    desc="square of DiamNac vs DiamProp",
-                )
-            
-                self.blockage_factor_interp.add_output(
-                    "blockage_factor",
-                    0.765,
-                    units="unitless",
-                    desc="blockage factor",
-                    training_data=np.array(
-                        [[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-                        [0.992, 0.991, 0.988, 0.983, 0.976, 0.970, 0.963],
-                        [0.986, 0.982, 0.977, 0.965, 0.953, 0.940, 0.927],
-                        [0.979, 0.974, 0.967, 0.948, 0.929, 0.908, 0.887],
-                        [0.972, 0.965, 0.955, 0.932, 0.905, 0.872, 0.835],
-                        [0.964, 0.954, 0.943, 0.912, 0.876, 0.834, 0.786],
-                        [0.955, 0.943, 0.928, 0.892, 0.848, 0.801, 0.751],
-                        [0.948, 0.935, 0.917, 0.872, 0.820, 0.763, 0.706],
-                        [0.940, 0.924, 0.902, 0.848, 0.790, 0.726, 0.662]]
-                        ),
-                )
-
-                self.add_subsystem(
-                    name='FT',
-                    subsys=om.ExecComp(
-                        'install_loss_factor = 1 - blockage_factor',
-                        blockage_factor={'units': 'unitless'},
-                        install_loss_factor={'units': 'unitless'},
-                    ),
-                    promotes_inputs=["blockage_factor"],
-                    promotes_outputs=[("install_loss_factor", Aircraft.Engine.INSTALLATION_LOSS_FACTOR)],
-                )
-            else:
-                comp = om.IndepVarComp(Aircraft.Engine.INSTALLATION_LOSS_FACTOR, val=0.0, units='unitless', lower=0.0, upper=1.0)
-                self.add_subsystem('input_install_loss', comp, promotes=['*'])
-
-
-            self.add_subsystem(
-                name='atmosphere',
-                subsys=USatm1976Comp(num_nodes=1),
-                promotes_inputs=[('h', Dynamic.Mission.ALTITUDE)],
+            self.blockage_factor_interp = self.add_subsystem(
+                "blockage_factor_interp",
+                om.MetaModelStructuredComp(method="scipy_slinear", extrapolate=True),
+                promotes_inputs=["sqa","equiv_adv_ratio"],
                 promotes_outputs=[
-                    ('sos', Dynamic.Mission.SPEED_OF_SOUND), ('rho', Dynamic.Mission.DENSITY),
-                    ('temp', Dynamic.Mission.TEMPERATURE), ('pres', Dynamic.Mission.STATIC_PRESSURE)],
+                    "blockage_factor",
+                ],
+            )
+
+            self.blockage_factor_interp.add_input(
+                "sqa",
+                0.0,
+                training_data=[0.00, 0.04, 0.08, 0.12, 0.16, 0.20, 0.24, 0.28, 0.32],
+                units="unitless",
+                desc="square of DiamNac/DiamProp",
+            )
+
+            self.blockage_factor_interp.add_input(
+                "equiv_adv_ratio",
+                0.0,
+                training_data=[0., 0.5, 1.0, 2.0, 3.0, 4.0, 5.0],
+                units="unitless",
+                desc="square of DiamNac vs DiamProp",
+            )
+            
+            self.blockage_factor_interp.add_output(
+                "blockage_factor",
+                0.765,
+                units="unitless",
+                desc="blockage factor",
+                training_data=np.array(
+                    [[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                    [0.992, 0.991, 0.988, 0.983, 0.976, 0.970, 0.963],
+                    [0.986, 0.982, 0.977, 0.965, 0.953, 0.940, 0.927],
+                    [0.979, 0.974, 0.967, 0.948, 0.929, 0.908, 0.887],
+                    [0.972, 0.965, 0.955, 0.932, 0.905, 0.872, 0.835],
+                    [0.964, 0.954, 0.943, 0.912, 0.876, 0.834, 0.786],
+                    [0.955, 0.943, 0.928, 0.892, 0.848, 0.801, 0.751],
+                    [0.948, 0.935, 0.917, 0.872, 0.820, 0.763, 0.706],
+                    [0.940, 0.924, 0.902, 0.848, 0.790, 0.726, 0.662]]
+                    ),
             )
 
             self.add_subsystem(
-                name='pre_hamilton_standard',
-                subsys=PreHamiltonStandard(),
-                promotes_inputs=[
-                    ("rho", Dynamic.Mission.DENSITY),
-                    ("temp", Dynamic.Mission.TEMPERATURE),
-                    Dynamic.Mission.VELOCITY,
-                    Aircraft.Engine.PROPELLER_TIP_SPEED,
-                    Aircraft.Engine.PROPELLER_DIAMETER,
-                    Dynamic.Mission.SHAFT_POWER,
-                ],
-                promotes_outputs=[
-                    Dynamic.Mission.MACH,
-                    "power_coefficient",
-                    "adv_ratio",
-                    "tip_mach",
-                    "density_ratio",
-                ])
+                name='FT',
+                subsys=om.ExecComp(
+                    'install_loss_factor = 1 - blockage_factor',
+                    blockage_factor={'units': 'unitless'},
+                    install_loss_factor={'units': 'unitless'},
+                ),
+                promotes_inputs=["blockage_factor"],
+                promotes_outputs=[("install_loss_factor", Aircraft.Engine.INSTALLATION_LOSS_FACTOR)],
+            )
+        else:
+            comp = om.IndepVarComp(Aircraft.Engine.INSTALLATION_LOSS_FACTOR, val=0.0, units='unitless', lower=0.0, upper=1.0)
+            self.add_subsystem('input_install_loss', comp, promotes=['*'])
 
-            self.add_subsystem(
-                name='hamilton_standard',
-                subsys=HamiltonStandard(num_blades=num_blades),
-                promotes_inputs=[
-                    Dynamic.Mission.MACH,
-                    "power_coefficient",
-                    "adv_ratio",
-                    "tip_mach",
-                    Aircraft.Engine.PROPELLER_ACTIVITY_FACTOR,
-                    Aircraft.Engine.PROPELLER_INTEGRATED_LIFT_COEFFICENT,
-                ],
-                promotes_outputs=[
-                    "thrust_coefficient",
-                    "ang_blade",
-                    "comp_tip_loss_factor",
-                ])
 
-            self.add_subsystem(
-                name='post_hamilton_standard',
-                subsys=PostHamiltonStandard(),
-                promotes_inputs=[
-                    "thrust_coefficient",
-                    "comp_tip_loss_factor",
-                    Aircraft.Engine.PROPELLER_TIP_SPEED,
-                    Aircraft.Engine.PROPELLER_DIAMETER,
-                    "density_ratio",
-                    Aircraft.Engine.INSTALLATION_LOSS_FACTOR,
-                    "adv_ratio",
-                    "power_coefficient",
-                ],
-                promotes_outputs=[
-                    "thrust_coefficient_comp_loss",
-                    "Thrust",
-                    "propeller_efficiency",
-                    "install_efficiency",
-                ])
+        self.add_subsystem(
+            name='atmosphere',
+            subsys=USatm1976Comp(num_nodes=1),
+            promotes_inputs=[('h', Dynamic.Mission.ALTITUDE)],
+            promotes_outputs=[
+                ('sos', Dynamic.Mission.SPEED_OF_SOUND), ('rho', Dynamic.Mission.DENSITY),
+                ('temp', Dynamic.Mission.TEMPERATURE), ('pres', Dynamic.Mission.STATIC_PRESSURE)],
+        )
+
+        self.add_subsystem(
+            name='pre_hamilton_standard',
+            subsys=PreHamiltonStandard(),
+            promotes_inputs=[
+                ("rho", Dynamic.Mission.DENSITY),
+                ("temp", Dynamic.Mission.TEMPERATURE),
+                Dynamic.Mission.VELOCITY,
+                Aircraft.Engine.PROPELLER_TIP_SPEED,
+                Aircraft.Engine.PROPELLER_DIAMETER,
+                Dynamic.Mission.SHAFT_POWER,
+            ],
+            promotes_outputs=[
+                Dynamic.Mission.MACH,
+                "power_coefficient",
+                "adv_ratio",
+                "tip_mach",
+                "density_ratio",
+            ])
+
+        self.add_subsystem(
+            name='hamilton_standard',
+            subsys=HamiltonStandard(num_blades=num_blades),
+            promotes_inputs=[
+                Dynamic.Mission.MACH,
+                "power_coefficient",
+                "adv_ratio",
+                "tip_mach",
+                Aircraft.Engine.PROPELLER_ACTIVITY_FACTOR,
+                Aircraft.Engine.PROPELLER_INTEGRATED_LIFT_COEFFICENT,
+            ],
+            promotes_outputs=[
+                "thrust_coefficient",
+                "ang_blade",
+                "comp_tip_loss_factor",
+            ])
+
+        self.add_subsystem(
+            name='post_hamilton_standard',
+            subsys=PostHamiltonStandard(),
+            promotes_inputs=[
+                "thrust_coefficient",
+                "comp_tip_loss_factor",
+                Aircraft.Engine.PROPELLER_TIP_SPEED,
+                Aircraft.Engine.PROPELLER_DIAMETER,
+                "density_ratio",
+                Aircraft.Engine.INSTALLATION_LOSS_FACTOR,
+                "adv_ratio",
+                "power_coefficient",
+            ],
+            promotes_outputs=[
+                "thrust_coefficient_comp_loss",
+                "Thrust",
+                "propeller_efficiency",
+                "install_efficiency",
+            ])
 
 
 if __name__ == "__main__":
@@ -260,6 +264,7 @@ if __name__ == "__main__":
     #pp.options.set(compute_installation_loss=True)
     options.set_val(Aircraft.Design.COMPUTE_INSTALLATION_LOSS,
                         val=True, units='unitless')
+    nflag = options.get_val(Aircraft.Design.COMPUTE_INSTALLATION_LOSS)
     prob.setup()
 
     print()
@@ -275,7 +280,7 @@ if __name__ == "__main__":
     prob.set_val(Aircraft.Engine.PROPELLER_TIP_SPEED, 800.0, units="ft/s")
     prob.set_val(Dynamic.Mission.SHAFT_POWER, 1850.0, units="hp")
 
-    print_report1(1, prob, num_blades)
+    print_report1(1, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
@@ -285,7 +290,7 @@ if __name__ == "__main__":
     prob.set_val(Aircraft.Engine.PROPELLER_TIP_SPEED, 800.0, units="ft/s")
     prob.set_val(Dynamic.Mission.SHAFT_POWER, 1850.0, units="hp")
 
-    print_report1(2, prob, num_blades)
+    print_report1(2, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
@@ -295,7 +300,7 @@ if __name__ == "__main__":
     prob.set_val(Aircraft.Engine.PROPELLER_TIP_SPEED, 750.0, units="ft/s")
     prob.set_val(Dynamic.Mission.SHAFT_POWER, 900.0, units="hp")
 
-    print_report1(3, prob, num_blades)
+    print_report1(3, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
@@ -303,10 +308,11 @@ if __name__ == "__main__":
     #pp.options.set(compute_installation_loss=False)
     options.set_val(Aircraft.Design.COMPUTE_INSTALLATION_LOSS,
                         val=False, units='unitless')
+    nflag = options.get_val(Aircraft.Design.COMPUTE_INSTALLATION_LOSS)
     prob.setup()
     prob.set_val(Aircraft.Engine.INSTALLATION_LOSS_FACTOR, 0.0, units="unitless")
     prob.set_val(Aircraft.Engine.PROPELLER_DIAMETER, 12.0, units="ft")
-    prob.set_val(Aircraft.Nacelle.AVG_DIAMETER, 2.4, units='ft')
+    #prob.set_val(Aircraft.Nacelle.AVG_DIAMETER, 2.4, units='ft')
     prob.set_val(Aircraft.Engine.PROPELLER_ACTIVITY_FACTOR, 150.0, units="unitless")
     prob.set_val(Aircraft.Engine.PROPELLER_INTEGRATED_LIFT_COEFFICENT, 0.5, units="unitless")
     #prob.set_val('DiamNac_DiamProp', 0.2, units="unitless")
@@ -315,14 +321,14 @@ if __name__ == "__main__":
     prob.set_val(Aircraft.Engine.PROPELLER_TIP_SPEED, 750.0, units="ft/s")
     prob.set_val(Dynamic.Mission.SHAFT_POWER, 1000.0, units="hp")
 
-    print_report1(4, prob, num_blades)
+    print_report1(4, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
     # Case 5
     prob.set_val(Aircraft.Engine.INSTALLATION_LOSS_FACTOR, 0.05, units="unitless")
 
-    print_report1(5, prob, num_blades)
+    print_report1(5, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
@@ -332,7 +338,7 @@ if __name__ == "__main__":
     prob.set_val(Aircraft.Engine.PROPELLER_TIP_SPEED, 785.0, units="ft/s")
     prob.set_val(Dynamic.Mission.SHAFT_POWER, 1250.0, units="hp")
 
-    print_report1(6, prob, num_blades)
+    print_report1(6, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
@@ -343,7 +349,7 @@ if __name__ == "__main__":
     prob.setup()
     prob.set_val(Aircraft.Engine.INSTALLATION_LOSS_FACTOR, 0.0, units="unitless")
     prob.set_val(Aircraft.Engine.PROPELLER_DIAMETER, 12.0, units="ft")
-    prob.set_val(Aircraft.Nacelle.AVG_DIAMETER, 2.4, units='ft')
+    #prob.set_val(Aircraft.Nacelle.AVG_DIAMETER, 2.4, units='ft')
     prob.set_val(Aircraft.Engine.PROPELLER_ACTIVITY_FACTOR, 150.0, units="unitless")
     prob.set_val(Aircraft.Engine.PROPELLER_INTEGRATED_LIFT_COEFFICENT, 0.5, units="unitless")
     #prob.set_val('DiamNac_DiamProp', 0.2, units="unitless")
@@ -352,13 +358,13 @@ if __name__ == "__main__":
     prob.set_val(Aircraft.Engine.PROPELLER_TIP_SPEED, 750.0, units="ft/s")
     prob.set_val(Dynamic.Mission.SHAFT_POWER, 1000.0, units="hp")
 
-    print_report1(7, prob, num_blades)
+    print_report1(7, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
     # Case 8
     prob.set_val(Aircraft.Engine.INSTALLATION_LOSS_FACTOR, 0.05, units="unitless")
-    print_report1(8, prob, num_blades)
+    print_report1(8, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
@@ -368,7 +374,7 @@ if __name__ == "__main__":
     prob.set_val(Aircraft.Engine.PROPELLER_TIP_SPEED, 785.0, units="ft/s")
     prob.set_val(Dynamic.Mission.SHAFT_POWER, 1250.0, units="hp")
 
-    print_report1(9, prob, num_blades)
+    print_report1(9, prob, num_blades, nflag)
     prob.run_model()
     print_report2(prob)
 
