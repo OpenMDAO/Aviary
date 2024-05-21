203cb72ca0da98dd002db44fb244650435efcd98
Herb Schilling <hschilling@nasa.gov>
2024-05-20 21:27:22 -0400

    pep8 errors

--- aviary/visualization/dashboard.py
+++ aviary/visualization/dashboard.py
@@ -415,7 +415,7 @@ def create_aircraft_3d_file(recorder_file, reports_dir, outfilepath):
     )
 
     aircraft_3d_model = Aircraft3DModel(recorder_file)
-    
+
     aircraft_3d_model.write_file(aircraft_3d_template_filepath, outfilepath)
 
 
