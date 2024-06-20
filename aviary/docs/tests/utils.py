a1cba71a5997b482d35c0824cb04a9f958945186
Carl Recine <carl.recine@nasa.gov>
2024-06-20 15:27:20 -0700

    added more testing utility functions and finished theory guide

--- aviary/docs/tests/utils.py
+++ aviary/docs/tests/utils.py
@@ -1,3 +1,15 @@
+import inspect
+
+
 def check_value(val1, val2):
-    if val1 != val2:
-        raise ValueError(f"{val1} is not {val2}")
+    if isinstance(val1, (str, int, float, list, tuple, dict)):
+        if val1 != val2:
+            raise ValueError(f"{val1} is not equal to {val2}")
+    else:
+        if val1 is not val2:
+            raise ValueError(f"{val1} is not {val2}")
+
+
+def check_args(func, expected_args: list, args_to_ignore=['self']):
+    args = [arg for arg in inspect.getfullargspec(func)[0] if arg not in args_to_ignore]
+    check_value(args.sort(), expected_args.sort())
