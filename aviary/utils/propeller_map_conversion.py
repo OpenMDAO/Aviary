8141366777d90c7c37ad9c6a5e4d37c363dc55e3
Xun Jiang <xun.j.jiang@nasa.gov>
2024-06-20 17:21:36 -0700

    move shared functions to conversion_utils.py

--- aviary/utils/propeller_map_conversion.py
+++ aviary/utils/propeller_map_conversion.py
@@ -2,12 +2,11 @@
 
 import argparse
 import getpass
-import itertools
 import numpy as np
 from datetime import datetime
 from enum import Enum, auto
-from pathlib import Path
 
+from aviary.utils.conversion_utils import conv_rep, conv_parse, read_map
 from aviary.api import NamedValues
 from aviary.utils.csv_data_file import write_data_file
 from aviary.utils.functions import get_path
@@ -124,8 +123,8 @@ def _read_gasp_propeller(fp, cmts):
 def _read_pm_header(f):
     """Read GASP propeller map header, returning the propeller scalars in a dict"""
     # parameter 2 is IPRINT in GASP and is ignored
-    iread, _ = _parse(
-        f, [*_rep(2, (int, 5))]
+    iread, _ = conv_parse(
+        f, [*conv_rep(2, (int, 5))]
     )
 
     return {
@@ -140,17 +139,18 @@ def _read_pm_table(f, cmts):
     and the final column for thrust coefficient.
     """
     tab_data = None
+    is_turbo_prop = True
 
     # table title
     title = f.readline().strip()
     cmts.append(f'# {title}')
     # number of maps in the table
-    (nmaps,) = _parse(f, [(int, 5)])
+    (nmaps,) = conv_parse(f, [(int, 5)])
     # blank line
     f.readline()
 
     for i in range(nmaps):
-        map_data = _read_map(f)
+        map_data = read_map(f, is_turbo_prop)
 
         # blank line following all but the last map in the table
         if i < nmaps - 1:
@@ -162,76 +162,6 @@ def _read_pm_table(f, cmts):
             tab_data = np.r_[tab_data, map_data]
 
     return tab_data
-
-
-def _rep(n, t):
-    """Shorthand for ``itertools.repeat`` with the multiplier first."""
-    return itertools.repeat(t, n)
-
-
-def _parse(f, fmt):
-    """Read a line from file ``f`` and parse it according to the given ``fmt``"""
-    return _strparse(f.readline(), fmt)
-
-
-def _strparse(s, fmt):
-    """Parse a string into fixed-width numeric fields.
-    ``fmt`` should be a list of tuples specifying (type, length) for each field in
-    string ``s``. Use None for the type to skip (i.e. not yield) that field.
-    """
-    p = 0
-    for typ, length in fmt:
-        sub = s[p: p + length]
-        if typ is not None:
-            yield typ(sub)
-        p += length
-
-
-def _read_map(f):
-    """Read a single map of a table from the propeller map file.
-    The map data is returned in the same format as in ``read_table``.
-    """
-    # map dimensions: FORMAT(/2I5,F10.1,10X))
-    npts, nline, amap = _parse(f, [*_rep(2, (int, 5)), (float, 10)])
-
-    map_data = np.empty((npts * nline, 4))
-    map_data[:, 0] = amap
-
-    # number of points on a single line - wrapped if more than 6
-    max_columns = 6
-
-    # point vals: FORMAT(10X,6F10.4,10X)
-    x = []
-    npts_remaining = npts
-    while npts_remaining > 0:
-        npts_to_read = min(max_columns, npts_remaining)
-        # remaining vals on wrapped line
-        x.extend(list(_parse(f, [(None, 10), *_rep(npts_to_read, (float, 10))])))
-        npts_remaining -= npts_to_read
-
-    map_data[:, 2] = np.tile(x, nline)
-
-    for j in range(nline):
-        npts_remaining = npts
-        npts_to_read = min(max_columns, npts_remaining)
-        # line (y) val then z vals: FORMAT(F10.4,6F10.1,10X,/(6F10.1,10X))
-        vals = list(_parse(f, [(float, 10), *_rep(npts_to_read, (float, 10))]))
-        y = vals[0]
-        z = vals[1:]
-        npts_remaining -= npts_to_read
-        while npts_remaining > 0:
-            npts_to_read = min(max_columns, npts_remaining)
-            # add remaining vals on warapped line
-            line_format = [*_rep(npts_to_read, (float, 10))]
-            line_format = [(None, 10), *line_format]
-            z.extend(list(_parse(f, line_format)))
-            npts_remaining -= npts_to_read
-
-        sl = slice(j * npts, (j + 1) * npts)
-        map_data[sl, 1] = y
-        map_data[sl, 3] = z
-
-    return map_data
 
 
 def _setup_PMC_parser(parser):
