1f1fb4515372771313340fdc826156e347838134
Xun Jiang <xun.j.jiang@nasa.gov>
2024-07-01 21:08:09 -0700

    autopep8

--- aviary/subsystems/propulsion/test/test_propeller_map.py
+++ aviary/subsystems/propulsion/test/test_propeller_map.py
@@ -15,9 +15,12 @@ class PropellerMapTest(unittest.TestCase):
         tol = 0.005
         aviary_options = get_option_defaults()
         prop_file_path = 'models/propellers/general_aviation.prop'
-        aviary_options.set_val(Aircraft.Engine.PROPELLER_DATA_FILE, val=prop_file_path, units='unitless')
-        aviary_options.set_val(Aircraft.Engine.INTERPOLATION_METHOD, val='slinear', units='unitless')
-        aviary_options.set_val(Aircraft.Engine.USE_PROPELLER_MAP, val=True, units='unitless')
+        aviary_options.set_val(
+            Aircraft.Engine.PROPELLER_DATA_FILE, val=prop_file_path, units='unitless')
+        aviary_options.set_val(
+            Aircraft.Engine.INTERPOLATION_METHOD, val='slinear', units='unitless')
+        aviary_options.set_val(
+            Aircraft.Engine.USE_PROPELLER_MAP, val=True, units='unitless')
         prop_model = PropellerMap('prop', aviary_options)
         prop_model.build_propeller_interpolator(3, aviary_options)
 
@@ -27,19 +30,21 @@ class PropellerMapTest(unittest.TestCase):
         y = prop_model.data[keys.CT]
         grid = np.array([x1, x2, x3]).T
         interp = InterpNDSemi(grid, y, method='slinear')
-        x = np.array([0.8, 0.025, 0.5])   # Mach, CP, J from general_aviation, expected CT: 0.0318
+        # Mach, CP, J from general_aviation, expected CT: 0.0318
+        x = np.array([0.8, 0.025, 0.5])
         f, df_dx = interp.interpolate(x, compute_derivative=True)
-        # print('value', f)
-        # print('derivative', df_dx)
         assert_near_equal(f, 0.0318, tolerance=tol)
 
     def test_propfan(self):
         tol = 0.005
         aviary_options = get_option_defaults()
         prop_file_path = 'models/propellers/PropFan.prop'
-        aviary_options.set_val(Aircraft.Engine.PROPELLER_DATA_FILE, val=prop_file_path, units='unitless')
-        aviary_options.set_val(Aircraft.Engine.INTERPOLATION_METHOD, val='slinear', units='unitless')
-        aviary_options.set_val(Aircraft.Engine.USE_PROPELLER_MAP, val=True, units='unitless')
+        aviary_options.set_val(
+            Aircraft.Engine.PROPELLER_DATA_FILE, val=prop_file_path, units='unitless')
+        aviary_options.set_val(
+            Aircraft.Engine.INTERPOLATION_METHOD, val='slinear', units='unitless')
+        aviary_options.set_val(
+            Aircraft.Engine.USE_PROPELLER_MAP, val=True, units='unitless')
         prop_model = PropellerMap('prop', aviary_options)
         prop_model.build_propeller_interpolator(3, aviary_options)
 
@@ -49,11 +54,11 @@ class PropellerMapTest(unittest.TestCase):
         y = prop_model.data[keys.CT]
         grid = np.array([x1, x2, x3]).T
         interp = InterpNDSemi(grid, y, method='slinear')
-        x = np.array([0.25, 0.0465, 0.2])  # Mach, CP, J from PropFan, expected CT: 0.095985
+        # Mach, CP, J from PropFan, expected CT: 0.095985
+        x = np.array([0.25, 0.0465, 0.2])
         f, df_dx = interp.interpolate(x, compute_derivative=True)
-        # print('value', f)
-        # print('derivative', df_dx)
         assert_near_equal(f, 0.095985, tolerance=tol)
+
 
 if __name__ == "__main__":
     unittest.main()
