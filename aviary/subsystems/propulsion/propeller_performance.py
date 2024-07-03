Fix Jacobian error. Make mach_type to be of OutMachType. Replace one IVC component by set_input_defaults

aviary/subsystems/propulsion/propeller_performance.py
@@ -11,7 +11,7 @@ from aviary.subsystems.propulsion.propeller_map import PropellerMap
 class OutMachs(om.ExplicitComponent):
     """This utility sets up relations among helical Mach, free stream Mach and propeller tip Mach.
     helical_mach = sqrt(mach^2 + tip_mach^2).
-    It compute the value of one from the inputs of the other two.
+    It computes the value of one from the inputs of the other two.
     """
 
     def initialize(self):
@@ -39,7 +39,7 @@ class OutMachs(om.ExplicitComponent):
                 "tip_mach",
                 val=np.zeros(nn),
                 units="unitless",
-                desc="tip Mach number at 3/4 of a blade",
+                desc="tip Mach number at 3/4 of radius of a blade",
             )
             self.add_output(
                 "helical_mach",
@@ -112,24 +112,24 @@ class OutMachs(om.ExplicitComponent):
         out_type = self.options["output_mach_type"]
 
         if out_type is OutMachType.HELICAL_MACH:
-            mach = inputs["MACH"]
+            mach = inputs["mach"]
             tip_mach = inputs["tip_mach"]
-            J["helical_mach", "MACH"] = -mach/np.sqrt(mach * mach + tip_mach * tip_mach)
-            J["helical_mach", "tip_mach"] = -tip_mach / \
+            J["helical_mach", "mach"] = mach/np.sqrt(mach * mach + tip_mach * tip_mach)
+            J["helical_mach", "tip_mach"] = tip_mach / \
                 np.sqrt(mach * mach + tip_mach * tip_mach)
         elif out_type is OutMachType.MACH:
             tip_mach = inputs["tip_mach"]
-            helical_mach = inputs["HELICAL_MACH"]
-            J["mach", "helical_mach"] = -helical_mach / \
+            helical_mach = inputs["helical_mach"]
+            J["mach", "helical_mach"] = helical_mach / \
                 np.sqrt(helical_mach * helical_mach - tip_mach * tip_mach)
-            J["mach", "tip_mach"] = tip_mach / \
+            J["mach", "tip_mach"] = -tip_mach / \
                 np.sqrt(helical_mach * helical_mach - tip_mach * tip_mach)
         elif out_type is OutMachType.TIP_MACH:
-            mach = inputs["MACH"]
+            mach = inputs["mach"]
             helical_mach = inputs["helical_mach"]
-            J["tip_mach", "helical_mach"] = -helical_mach / \
+            J["tip_mach", "helical_mach"] = helical_mach / \
                 np.sqrt(helical_mach * helical_mach - mach * mach)
-            J["tip_mach", "MACH"] = mach / \
+            J["tip_mach", "mach"] = -mach / \
                 np.sqrt(helical_mach * helical_mach - mach * mach)
 
 
@@ -251,8 +251,8 @@ class InstallLoss(om.Group):
 
 class PropellerPerformance(om.Group):
     """
-    Computation of propeller thrust coefficient based on Hamilton Standard or a user
-    provided propeller map. Note that a propeller map allows either helical Mach number or
+    Computation of propeller thrust coefficient based on the Hamilton Standard model or a user
+    provided propeller map. Note that a propeller map allows either the helical Mach number or
     free stream Mach number as input. This infomation will be detected automatically when the 
     propeller map is loaded into memory.
     The installation loss factor is either a user input or computed internally.
@@ -352,8 +352,7 @@ class PropellerPerformance(om.Group):
                 Aircraft.Engine.PROPELLER_DATA_FILE)
             print(f"prop_file_path: {prop_file_path}")
             mach_type = prop_model.read_and_set_mach_type(prop_file_path)
-            print(f"mach type: {mach_type}")
-            if mach_type == 'helical_mach':
+            if mach_type == OutMachType.HELICAL_MACH:
                 self.add_subsystem(
                     name='selectedMach',
                     subsys=OutMachs(
@@ -387,10 +386,11 @@ class PropellerPerformance(om.Group):
                 ])
 
             # propeller map has taken compresibility into account.
-            IVC = om.IndepVarComp("comp_tip_loss_factor",
-                                  np.linspace(1.0, 1.0, nn),
-                                  units='unitless')
-            self.add_subsystem('IVC', IVC, promotes=['comp_tip_loss_factor'])
+            #IVC = om.IndepVarComp("comp_tip_loss_factor",
+            #                      np.linspace(1.0, 1.0, nn),
+            #                      units='unitless')
+            #self.add_subsystem('IVC', IVC, promotes=['comp_tip_loss_factor'])
+            self.set_input_defaults('comp_tip_loss_factor', np.linspace(1.0, 1.0, nn), units='unitless')
         else:
             self.add_subsystem(
                 name='hamilton_standard',