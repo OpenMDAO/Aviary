7be24760c9ffad7c39e281b6de5ca9f6095b7642
Jatin Soni <jatinsoni006@gmail.com>
2024-08-07 15:38:27 -0400

    updated output printing, added easy option to optimize all altitudes/machs

--- multi_aviary_copymodel.py
+++ multi_aviary_copymodel.py
@@ -67,12 +67,18 @@ class MultiMissionProblem(om.Problem):
             self.fuel_vars.append(promoted_name)
             self.model.add_subsystem(
                 self.group_prefix + f'_{i}', prob.model,
-                promotes=[Mission.Design.GROSS_MASS,
-                          Mission.Design.RANGE,
-                          (Mission.Objectives.FUEL, promoted_name)])
+                promotes_inputs=[Mission.Design.GROSS_MASS,
+                                 Mission.Design.RANGE,
+                                 Aircraft.Wing.SPAN,
+                                 Aircraft.Wing.AREA],
+                promotes_outputs=[(Mission.Objectives.FUEL, promoted_name)])
 
     def add_design_variables(self):
-        self.model.add_design_var('mission:design:gross_mass', lower=10., upper=900e3)
+        self.model.add_design_var(Mission.Design.GROSS_MASS,
+                                  lower=10., upper=900e3, units='lbm')
+        self.model.add_design_var(Aircraft.Wing.SPAN, lower=100., upper=500., units='ft')
+        self.model.add_design_var(Aircraft.Wing.AREA, lower=10.,
+                                  upper=1e6, units='ft**2')
 
     def add_driver(self):
         self.driver = om.pyOptSparseDriver()
@@ -142,7 +148,7 @@ class MultiMissionProblem(om.Problem):
                         units=unit)
                     time = np.hstack([time, np.ndarray.flatten(rawt)])
                     yvar = np.hstack([yvar, np.ndarray.flatten(rawy)])
-                plt.plot(time, yvar, 'o')
+                plt.plot(time, yvar, linewidth=self.num_missions-i)
             plt.xlabel("Time (s)")
             plt.ylabel(f"{var.title()} ({unit})")
             plt.grid()
@@ -181,11 +187,19 @@ class MultiMissionProblem(om.Problem):
         """Specify vars with name and unit in a tuple, e.g. vars = [ (Mission.Summary.FUEL_BURNED, 'lbm') ]"""
 
         print("\n\n=========================\n")
+        print(f"{'':40}", end=': ')
+        for i in range(self.num_missions):
+            name = f"Mission {i}"
+            print(f"{name:^30}", end='| ')
+        print()
         for var, unit in vars:
-            print(f"Variable: {var}")
+            varname = f"Variable: {var.replace(':','.').upper()}"
+            print(f"{varname:40}", end=": ")
             for i in range(self.num_missions):
                 val = self.get_val(f'group_{i}.{var}', units=unit)[0]
-                print(f"\tPlane {i}: {val} ({unit})")
+                printstatement = f"{val} ({unit})"
+                print(f"{printstatement:^30}", end="| ")
+            print()
 
 
 def C5_example(makeN2=False):
@@ -194,6 +208,13 @@ def C5_example(makeN2=False):
     planes = [join(plane_dir, plane) for plane in planes]
     phase_infos = [c5_maxpayload_phase_info,
                    c5_intermediate_phase_info, c5_ferry_phase_info]
+    optalt, optmach = False, False
+    for phaseinfo in phase_infos:
+        for key in phaseinfo.keys():
+            if "user_options" in phaseinfo[key].keys():
+                phaseinfo[key]["user_options"]["optimize_mach"] = optmach
+                phaseinfo[key]["user_options"]["optimize_altitude"] = optalt
+
     weights = [1, 1, 1]
 
     super_prob = MultiMissionProblem(planes, phase_infos, weights)
@@ -215,14 +236,17 @@ def C5_example(makeN2=False):
     super_prob.run()
     printoutputs = [(Aircraft.Design.EMPTY_MASS, 'lbm'),
                     (Mission.Summary.FUEL_BURNED, 'lbm'),
-                    (Mission.Summary.GROSS_MASS, 'lbm')]
+                    (Mission.Summary.GROSS_MASS, 'lbm'),
+                    (Aircraft.Wing.SPAN, 'ft'),
+                    (Aircraft.Wing.AREA, 'ft**2')]
     super_prob.print_vars(vars=printoutputs)
 
     plotvars = [('altitude', 'ft'),
                 ('mass', 'lbm'),
                 ('drag', 'lbf'),
                 ('distance', 'nmi'),
-                ('throttle', 'unitless')]
+                ('throttle', 'unitless'),
+                ('mach', 'unitless')]
     super_prob.create_timeseries_plots(plotvars=plotvars, show=False)
 
     super_prob.create_payload_range_plot(show=False)
@@ -237,27 +261,102 @@ if __name__ == '__main__':
     super_prob = C5_example(makeN2=makeN2)
 
 """
-1:1
-Variable: mission:summary:fuel_burned
-Values: 164988.61664962117, 306345.04737967893 (lbm)
+1:1:1 and 5:1:1 and 1:1:5 and 1:5:1
 Variable: aircraft:design:empty_mass
-Values: 378204.91862045845, 378204.91862045845 (lbm)
+        Plane 0: 377110.8519657982 (lbm),       Plane 1: 377110.8519657982 (lbm),       Plane 2: 377110.8519657982 (lbm),
+Variable: mission:summary:fuel_burned
+        Plane 0: 167530.46091225138 (lbm),      Plane 1: 308428.65141334233 (lbm),      Plane 2: 412689.6603555365 (lbm),
 Variable: mission:summary:gross_mass
-Values: 598462.8871182877, 710244.3178483456 (lbm)
+        Plane 0: 599904.1437916472 (lbm),       Plane 1: 711227.3342927382 (lbm),       Plane 2: 793323.6057604996 (lbm),
+Variable: aircraft:wing:span
+        Plane 0: 235.7734017415966 (ft),        Plane 1: 235.7734017415966 (ft),        Plane 2: 235.7734017415966 (ft),
+Variable: aircraft:wing:area
+        Plane 0: 6106.843788863166 (ft**2),     Plane 1: 6106.843788863166 (ft**2),     Plane 2: 6106.843788863166 (ft**2),
 
-1.5:1
-Variable: mission:summary:fuel_burned
-Values: 164988.61476287164, 306345.04738991836 (lbm)
-Variable: aircraft:design:empty_mass
-Values: 378204.91862045845, 378204.91862045845 (lbm)
-Variable: mission:summary:gross_mass
-Values: 598462.8852315382, 710244.3178585849 (lbm)
+1:1:5
 
-2:1
-Variable: mission:summary:fuel_burned
-Values: 164988.61651039496, 306345.04738988867 (lbm)
+"""
+
+"""
+inter payload: 100k
 Variable: aircraft:design:empty_mass
-Values: 378204.91862045845, 378204.91862045845 (lbm)
+        Plane 0: 374179.5394606042 (lbm)
+        Plane 1: 374179.5394606042 (lbm)
+        Plane 2: 374179.5394606042 (lbm)
+Variable: mission:summary:fuel_burned
+        Plane 0: 165739.44323952997 (lbm)
+        Plane 1: 306588.0148270305 (lbm)
+        Plane 2: 411012.49583405076 (lbm)
 Variable: mission:summary:gross_mass
-Values: 598462.8869790616, 710244.3178585552 (lbm)
+        Plane 0: 595188.3345483424 (lbm)
+        Plane 1: 702786.9061358428 (lbm)
+        Plane 2: 788721.6496684301 (lbm)
+
+inter payload: 140k
+Variable: aircraft:design:empty_mass
+        Plane 0: 374179.53946059936 (lbm)
+        Plane 1: 374179.53946059936 (lbm)
+        Plane 2: 374179.53946059936 (lbm)
+Variable: mission:summary:fuel_burned
+        Plane 0: 165739.44340813044 (lbm)
+        Plane 1: 306588.0148359792 (lbm)
+        Plane 2: 411012.4960230509 (lbm)
+Variable: mission:summary:gross_mass
+        Plane 0: 595188.3347169379 (lbm)
+        Plane 1: 710136.9061447867 (lbm)
+        Plane 2: 788721.6498574257 (lbm)
+
+140k inter, 301k max
+Variable: aircraft:design:empty_mass
+        Plane 0: 374179.5394605969 (lbm)
+        Plane 1: 374179.5394605969 (lbm)
+        Plane 2: 374179.5394605969 (lbm)
+Variable: mission:summary:fuel_burned
+        Plane 0: 165739.44397328247 (lbm)
+        Plane 1: 306588.01484213956 (lbm)
+        Plane 2: 411012.4961383113 (lbm)
+Variable: mission:summary:gross_mass
+        Plane 0: 598863.3352823037 (lbm)
+        Plane 1: 710136.9061509445 (lbm)
+        Plane 2: 788721.6499726834 (lbm)
+
+140k inter, 301k max, 10k ferry
+Variable: aircraft:design:empty_mass
+        Plane 0: 374179.5394605957 (lbm)
+        Plane 1: 374179.5394605957 (lbm)
+        Plane 2: 374179.5394605957 (lbm)
+Variable: mission:summary:fuel_burned
+        Plane 0: 165739.4440247023 (lbm)
+        Plane 1: 306588.01484336343 (lbm)
+        Plane 2: 411012.49619538186 (lbm)
+Variable: mission:summary:gross_mass
+        Plane 0: 598863.3353337225 (lbm)
+        Plane 1: 710136.9061521672 (lbm)
+        Plane 2: 790586.3875041857 (lbm)
+"""
+
+"""
+optimize mach only:                                 optimize both mach and alt:                         optimize neither mach or altitude
+Variable: aircraft:design:empty_mass                Variable: aircraft:design:empty_mass                Variable: aircraft:design:empty_mass
+        Plane 0: 326047.42156711605 (lbm)                 Plane 0: 390475.71469928196 (lbm)                 Plane 0: 371320.23571631836 (lbm)
+        Plane 1: 326047.42156711605 (lbm)                 Plane 1: 390475.71469928196 (lbm)                 Plane 1: 371320.23571631836 (lbm)
+        Plane 2: 326047.42156711605 (lbm)                 Plane 2: 390475.71469928196 (lbm)                 Plane 2: 371320.23571631836 (lbm)
+Variable: mission:summary:fuel_burned               Variable: mission:summary:fuel_burned               Variable: mission:summary:fuel_burned
+        Plane 0: 175717.28062873823 (lbm)                 Plane 0: 178314.99288207013 (lbm)                 Plane 0: 167946.2102766974 (lbm)
+        Plane 1: 306419.71885746776 (lbm)                 Plane 1: 325789.9474922766 (lbm)                  Plane 1: 309094.299337939 (lbm)
+        Plane 2: 394791.91474366543 (lbm)                 Plane 2: 429716.0286375711 (lbm)                  Plane 2: 413746.64501453174 (lbm)
+Variable: mission:summary:gross_mass                Variable: mission:summary:gross_mass                Variable: mission:summary:gross_mass
+        Plane 0: 557034.0540440837 (lbm)                  Plane 0: 624060.059429561 (lbm)                   Plane 0: 594535.797841224 (lbm)
+        Plane 1: 658161.4922728132 (lbm)                  Plane 1: 741960.0140397676 (lbm)                  Plane 1: 706108.8869024655 (lbm)
+        Plane 2: 724368.9506845778 (lbm)                  Plane 2: 823721.3577106291 (lbm)                  Plane 2: 788596.4951046254 (lbm)
+Variable: aircraft:wing:span                        Variable: aircraft:wing:span                        Variable: aircraft:wing:span
+        Plane 0: 54.707828078624665 (ft)                  Plane 0: 287.14946969690783 (ft)                  Plane 0: 211.8281919608065 (ft)
+        Plane 1: 54.707828078624665 (ft)                  Plane 1: 287.14946969690783 (ft)                  Plane 1: 211.8281919608065 (ft)
+        Plane 2: 54.707828078624665 (ft)                  Plane 2: 287.14946969690783 (ft)                  Plane 2: 211.8281919608065 (ft)
+Variable: aircraft:wing:aspect_ratio                Variable: aircraft:wing:aspect_ratio                Variable: aircraft:wing:aspect_ratio
+        Plane 0: 6.263196240285538 (unitless)             Plane 0: 6.931291416800268 (unitless)             Plane 0: 7.576574965501183 (unitless)
+        Plane 1: 6.263196240285538 (unitless)             Plane 1: 6.931291416800268 (unitless)             Plane 1: 7.576574965501183 (unitless)
+        Plane 2: 6.263196240285538 (unitless)             Plane 2: 6.931291416800268 (unitless)             Plane 2: 7.576574965501183 (unitless)
+
+
 """
