f307aa94eeb14a5c1a3c33b97afed562a4e155e6
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2024-08-12 15:19:38 -0400

    PEP

--- aviary/mission/flops_based/phases/detailed_landing_phases.py
+++ aviary/mission/flops_based/phases/detailed_landing_phases.py
@@ -1259,7 +1259,9 @@ class LandingTrajectory:
                                            BaseMetaData,
                                            LegacyCode('FLOPS'))
 
-            phase_info = {'subsystem_options':{'core_aerodynamics':{'method':'low_speed'}}}
+            phase_info = {'subsystem_options':{
+                'core_aerodynamics':{'method':'low_speed'}}
+            }
 
             params = aero.get_parameters(aviary_options, phase_info)
 
