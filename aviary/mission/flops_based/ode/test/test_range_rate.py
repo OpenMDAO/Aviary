e4ea79430638dff679e7c417ce700e7484329c91
Xun Jiang <xun.j.jiang@nasa.gov>
2024-08-14 15:24:35 -0700

    minor update

--- aviary/mission/flops_based/ode/test/test_range_rate.py
+++ aviary/mission/flops_based/ode/test/test_range_rate.py
@@ -13,7 +13,7 @@ from aviary.variable_info.variables import Dynamic
 class RangeRateTest(unittest.TestCase):
     def setUp(self):
         """
-        test using data from validation_cases/validation_data/full_mission_test_data.py
+        test using data from validation_cases/validation_data/flops_data/full_mission_test_data.py
         """
 
         prob = self.prob = om.Problem()
