72267bcab384c840f25a8cb7481f4f67a2fb5818
Xun Jiang <xun.j.jiang@nasa.gov>
2024-08-14 09:54:47 -0700

    add and comment out test_IO(). If it is run, it will fail. It might be useful in the future.

--- aviary/mission/flops_based/ode/test/test_required_thrust.py
+++ aviary/mission/flops_based/ode/test/test_required_thrust.py
@@ -6,18 +6,14 @@ from openmdao.utils.assert_utils import (assert_check_partials,
                                          assert_near_equal)
 
 from aviary.mission.flops_based.ode.required_thrust import RequiredThrust
-from aviary.models.N3CC.N3CC_data import (
-    detailed_takeoff_climbing, detailed_takeoff_ground, inputs)
-from aviary.validation_cases.validation_tests import do_validation_test
-from aviary.variable_info.variables import Dynamic, Mission
+from aviary.utils.test_utils.variable_test import assert_match_varnames
+from aviary.variable_info.variables import Dynamic
 
- 
-class TakeoffEOMTest(unittest.TestCase):
 
-    def test_case(self):
+class RequiredThrustTest(unittest.TestCase):
 
-        tol = 1e-6
-        prob = om.Problem()
+    def setUp(self):
+        prob = self.prob = om.Problem()
         prob.model.add_subsystem(
             "range_rate", RequiredThrust(num_nodes=2), promotes=["*"]
         )
@@ -38,15 +34,23 @@ class TakeoffEOMTest(unittest.TestCase):
         )
 
         prob.setup(check=False, force_alloc_complex=True)
-        prob.run_model()
+
+    def test_case(self):
+
+        tol = 1e-6
+        self.prob.run_model()
 
         assert_near_equal(
-            prob["thrust_required"],
+            self.prob["thrust_required"],
             np.array([304653.8, 208303.1]), tol
         )
 
-        partial_data = prob.check_partials(out_stream=None, method="cs")
+        partial_data = self.prob.check_partials(out_stream=None, method="cs")
         assert_check_partials(partial_data, atol=1e-10, rtol=1e-10)
+
+    # TODO: this test fails: The outputs {'thrust_required'} in the provided subsystem are not found in the provided variable structure.
+    # def test_IO(self):
+    #     assert_match_varnames(self.prob.model)
 
 
 if __name__ == "__main__":
