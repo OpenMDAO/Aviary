50285f7af506dff88692df12278be28e332aec2b
Taha Itani <92129997+tahaitani@users.noreply.github.com>
2024-08-26 10:31:52 -0400

    Update plot_drag_polar.py

--- aviary/interface/plot_drag_polar.py
+++ aviary/interface/plot_drag_polar.py
@@ -14,7 +14,7 @@ def plot_drag_polar(input_file=None):
         input_file = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
         if not input_file:
             messagebox.showerror(
-            "Error", "No file selected, using default data in test_drag_polar_data.csv")
+                "Error", "No file selected, using default data in test_drag_polar_data.csv")
             input_file = get_path('utils/test/test_drag_polar_data.csv')
 
     try:
