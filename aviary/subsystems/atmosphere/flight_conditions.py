1d1468e6e47cdcd3086f69ced89f8fdef01741a5
Xun Jiang <xun.j.jiang@nasa.gov>
2024-09-06 12:31:10 -0700

    autopep8

--- aviary/subsystems/atmosphere/flight_conditions.py
+++ aviary/subsystems/atmosphere/flight_conditions.py
@@ -6,7 +6,7 @@ from aviary.variable_info.enums import SpeedType
 from aviary.variable_info.variables import Dynamic
 
 
-class FlightConditions(om.ExplicitComponent): 
+class FlightConditions(om.ExplicitComponent):
     """
     Flight conditions. Given a speed type (TAS, SPEED_OF_SOUND and EAS) and air density,
     compute dynamic pressure and Mach number.
