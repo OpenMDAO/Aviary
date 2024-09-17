a73719bd5f5d875a0899878b0be9fadd9f8e3aa8
Xun Jiang <xun.j.jiang@nasa.gov>
2024-09-17 15:49:27 -0700

    working in progress: modify the docstring in test_climb_constraints.py

--- aviary/mission/gasp_based/ode/constraints/test/test_climb_constraints.py
+++ aviary/mission/gasp_based/ode/constraints/test/test_climb_constraints.py
@@ -13,7 +13,7 @@ from aviary.variable_info.variables import Dynamic
 
 class SpeedConstraintTestCase1(unittest.TestCase):
     """
-    Test speed constraint at MACH = 0.8 with flight MACH at 0.6
+    Test speed constraint at MACH = 0.6 with targeted MACH at 0.8
     """
 
     def setUp(self):
@@ -49,7 +49,7 @@ class SpeedConstraintTestCase1(unittest.TestCase):
 
 class SpeedConstraintTestCase2(unittest.TestCase):
     """
-    Test speed constraint at MACH = 0.9
+    Test speed constraint at MACH = 0.9 with targeted Mach at 0.8
     """
 
     def setUp(self):
