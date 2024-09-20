984255a113cd2b9655dbd6ac78a871ffb612e983
Xun Jiang <xun.j.jiang@nasa.gov>
2024-09-20 16:13:53 -0700

    order imports in benchmark_tests

--- aviary/validation_cases/benchmark_tests/test_0_iters.py
+++ aviary/validation_cases/benchmark_tests/test_0_iters.py
@@ -3,9 +3,9 @@ import unittest
 
 from openmdao.utils.testing_utils import require_pyoptsparse, use_tempdirs
 
-from aviary.interface.methods_for_level2 import AviaryProblem
 from aviary.interface.default_phase_info.two_dof import phase_info as two_dof_phase_info
 from aviary.interface.default_phase_info.height_energy import phase_info as height_energy_phase_info
+from aviary.interface.methods_for_level2 import AviaryProblem
 from aviary.models.N3CC.N3CC_data import inputs
 
 
