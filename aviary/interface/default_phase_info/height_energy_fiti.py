85f94f016a6a4ed69800ee638c6e42e54e6777a4
Xun Jiang <xun.j.jiang@nasa.gov>
2024-09-20 13:43:54 -0700

    add_default_sgm_args is needed even though it is not used in height_energy_fiti.py

--- aviary/interface/default_phase_info/height_energy_fiti.py
+++ aviary/interface/default_phase_info/height_energy_fiti.py
@@ -1,8 +1,9 @@
 from aviary.mission.flops_based.phases.time_integration_phases import SGMHeightEnergy
 from aviary.utils.aviary_values import AviaryValues
-from aviary.variable_info.enums import SpeedType, AlphaModes
 from aviary.variable_info.variables import Dynamic, Mission
+from aviary.variable_info.enums import SpeedType, AlphaModes
 
+from aviary.interface.default_phase_info.two_dof_fiti import add_default_sgm_args
 
 # defaults for height energy based forward in time integeration phases
 cruise_mach = .8,
