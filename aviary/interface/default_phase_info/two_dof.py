842d234f948f2c578670b99b682d12bc65a062a3
Bennett <cmbenne3@ndc.nasa.gov>
2024-09-29 23:33:47 -0400

    update 2dof phase info parameterization

--- aviary/interface/default_phase_info/two_dof.py
+++ aviary/interface/default_phase_info/two_dof.py
@@ -350,17 +350,18 @@ def phase_info_parameterization(phase_info, post_mission_info, aviary_inputs):
         phase_info['ascent']['initial_guesses']['mass'] = \
             ([gross_mass, gross_mass], 'lbm')
 
-        phase_info['cruise']['initial_guesses']['mass'] = \
-            ([gross_mass, -fuel_used], 'lbm')
-
         end_mass = gross_mass - fuel_used
-        phase_info['desc1']['initial_guesses']['mass'] = (end_mass, 'lbm')
-        phase_info['desc2']['initial_guesses']['mass'] = (end_mass, 'lbm')
+        phase_info['cruise']['initial_guesses']['mass'] = \
+            ([gross_mass, end_mass], 'lbm')
+
+        phase_info['desc1']['initial_guesses']['mass'] = ([end_mass, end_mass] 'lbm')
+        phase_info['desc2']['initial_guesses']['mass'] = ([end_mass, end_mass] 'lbm')
 
     # Mach
     old_mach_cruise = phase_info['cruise']['initial_guesses']['mach'][0]
     if mach_cruise != old_mach_cruise:
 
-        phase_info['cruise']['initial_guesses']['mach'] = (mach_cruise, 'unitless')
+        phase_info['cruise']['initial_guesses']['mach'] = (
+            [mach_cruise, mach_cruise], 'unitless')
 
     return phase_info, post_mission_info
