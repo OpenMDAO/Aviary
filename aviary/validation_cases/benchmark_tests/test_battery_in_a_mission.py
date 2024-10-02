1edaf65862a68e7d8a84d7ab78a835645179742e
Kaushik Ponnapalli <kaushik.s.ponnapalli@nasa.gov>
2024-10-02 11:56:39 -0500

    Updated values for tests

--- aviary/validation_cases/benchmark_tests/test_battery_in_a_mission.py
+++ aviary/validation_cases/benchmark_tests/test_battery_in_a_mission.py
@@ -88,8 +88,8 @@ class TestSubsystemsMission(unittest.TestCase):
         fuel_burned = prob.get_val(av.Mission.Summary.FUEL_BURNED, units='lbm')
 
         # Check outputs
-        assert_near_equal(electric_energy_used[-1], 42.5594728, 1.e-7)
-        assert_near_equal(fuel_burned, 805.8963261, 1.e-7)
+        assert_near_equal(electric_energy_used[-1], 38.60538132, 1.e-7)
+        assert_near_equal(fuel_burned, 676.87235486, 1.e-7)
 
 
 if __name__ == "__main__":
