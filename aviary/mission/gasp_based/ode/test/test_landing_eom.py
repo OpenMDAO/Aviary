7dd03b92db9882c90be971409be77f4c10fb9ccb
Xun Jiang <xun.j.jiang@nasa.gov>
2024-10-04 14:55:07 -0700

    work in progress: vectorize taxi and landing in GASP

---
+++ aviary/mission/gasp_based/ode/test/test_landing_eom.py
@@ -0,0 +1,248 @@
+import unittest
+
+import openmdao.api as om
+from openmdao.utils.assert_utils import (assert_check_partials,
+                                         assert_near_equal)
+
+from aviary.constants import RHO_SEA_LEVEL_ENGLISH
+from aviary.mission.gasp_based.ode.landing_eom import (
+    GlideConditionComponent, LandingAltitudeComponent,
+    LandingGroundRollComponent)
+from aviary.variable_info.variables import Aircraft, Mission
+
+
+class LandingAltTestCase(unittest.TestCase):
+    def setUp(self):
+
+        self.prob = om.Problem()
+        self.prob.model.add_subsystem(
+            "group", LandingAltitudeComponent(), promotes=["*"]
+        )
+
+        self.prob.model.set_input_defaults(
+            Mission.Landing.OBSTACLE_HEIGHT, 50, units="ft")
+        self.prob.model.set_input_defaults(
+            Mission.Landing.AIRPORT_ALTITUDE, 0, units="ft")
+
+        self.prob.setup(check=False, force_alloc_complex=True)
+
+    def test_case1(self):
+        tol = 1e-6
+        self.prob.run_model()
+
+        assert_near_equal(
+            self.prob[Mission.Landing.OBSTACLE_HEIGHT], 50, tol
+        )  # not actual GASP value, but intuitively correct
+
+        partial_data = self.prob.check_partials(out_stream=None, method="cs")
+        assert_check_partials(partial_data, atol=1e-12, rtol=1e-12)
+
+
+class GlideTestCase(unittest.TestCase):
+    def setUp(self):
+
+        self.prob = om.Problem()
+        self.prob.model.add_subsystem(
+            "group", GlideConditionComponent(), promotes=["*"]
+        )
+
+        self.prob.model.set_input_defaults(
+            "rho_app", RHO_SEA_LEVEL_ENGLISH, units="slug/ft**3"
+        )  # value from online calculator
+
+        self.prob.model.set_input_defaults(
+            Mission.Landing.MAXIMUM_SINK_RATE, 900, units="ft/min")
+
+        self.prob.model.set_input_defaults("mass", 165279, units="lbm")
+        self.prob.model.set_input_defaults(Aircraft.Wing.AREA, 1370.3, units="ft**2")
+        self.prob.model.set_input_defaults(
+            Mission.Landing.GLIDE_TO_STALL_RATIO, 1.3, units="unitless")
+        self.prob.model.set_input_defaults("CL_max", 2.9533, units="unitless")
+        self.prob.model.set_input_defaults(
+            Mission.Landing.MAXIMUM_FLARE_LOAD_FACTOR, 1.15, units="unitless"
+        )
+        self.prob.model.set_input_defaults(
+            Mission.Landing.TOUCHDOWN_SINK_RATE, 5, units="ft/s")
+        self.prob.model.set_input_defaults(
+            Mission.Landing.INITIAL_ALTITUDE, val=50.0, units="ft")
+        self.prob.model.set_input_defaults(
+            Mission.Landing.BRAKING_DELAY, val=1.0, units="s")
+
+        self.prob.setup(check=False, force_alloc_complex=True)
+
+    def test_case1(self):
+        tol = 1e-6
+        self.prob.run_model()
+
+        assert_near_equal(
+            self.prob.get_val(Mission.Landing.INITIAL_VELOCITY,
+                              units="kn"), 142.783, tol
+        )  # note: actual GASP value is: 142.74
+        assert_near_equal(
+            self.prob.get_val(Mission.Landing.STALL_VELOCITY,
+                              units="kn"), 109.8331, tol
+        )  # note: EAS in GASP, although at this altitude they are nearly identical. actual GASP value is 109.73
+        assert_near_equal(
+            self.prob.get_val("TAS_touchdown", units="kn"), 126.3081, tol
+        )  # note: actual GASP value is: 126.27
+        assert_near_equal(
+            self.prob.get_val("density_ratio", units="unitless"), 1.0, tol
+        )  # note: calculated from GASP glide speed values as: .998739
+        assert_near_equal(
+            self.prob.get_val("wing_loading_land", units="lbf/ft**2"), 120.61519375, tol
+        )  # note: actual GASP value is: 120.61
+        assert_near_equal(
+            self.prob.get_val("theta", units="deg"), 3.56857, tol
+        )  # note: actual GASP value is: 3.57
+        assert_near_equal(
+            self.prob.get_val("glide_distance", units="ft"), 801.7444, tol
+        )  # note: actual GASP value is: 802
+        assert_near_equal(
+            self.prob.get_val("tr_distance", units="ft"), 166.5303, tol
+        )  # note: actual GASP value is: 167
+        assert_near_equal(
+            self.prob.get_val("delay_distance", units="ft"), 213.184, tol
+        )  # note: actual GASP value is: 213
+        assert_near_equal(
+            self.prob.get_val("flare_alt", units="ft"), 20.73407, tol
+        )  # note: actual GASP value is: 20.8
+
+        partial_data = self.prob.check_partials(out_stream=None, method="cs")
+        assert_check_partials(partial_data, atol=1e-10, rtol=1e-12)
+
+
+class GlideTestCase2(unittest.TestCase):
+    """
+    Test mass-weight conversion
+    """
+
+    def setUp(self):
+        import aviary.mission.gasp_based.phases.landing_components as landing
+        landing.GRAV_ENGLISH_LBM = 1.1
+
+    def tearDown(self):
+        import aviary.mission.gasp_based.phases.landing_components as landing
+        landing.GRAV_ENGLISH_LBM = 1.0
+
+    def test_case1(self):
+        prob = om.Problem()
+        prob.model.add_subsystem(
+            "group", GlideConditionComponent(), promotes=["*"])
+        prob.model.set_input_defaults(
+            "rho_app", RHO_SEA_LEVEL_ENGLISH, units="slug/ft**3")
+        prob.model.set_input_defaults(
+            Mission.Landing.MAXIMUM_SINK_RATE, 900, units="ft/min")
+        prob.model.set_input_defaults("mass", 165279, units="lbm")
+        prob.model.set_input_defaults(Aircraft.Wing.AREA, 1370.3, units="ft**2")
+        prob.model.set_input_defaults(
+            Mission.Landing.GLIDE_TO_STALL_RATIO, 1.3, units="unitless")
+        prob.model.set_input_defaults("CL_max", 2.9533, units="unitless")
+        prob.model.set_input_defaults(
+            Mission.Landing.MAXIMUM_FLARE_LOAD_FACTOR, 1.15, units="unitless")
+        prob.model.set_input_defaults(
+            Mission.Landing.TOUCHDOWN_SINK_RATE, 5, units="ft/s")
+        prob.model.set_input_defaults(
+            Mission.Landing.INITIAL_ALTITUDE, val=50.0, units="ft")
+        prob.model.set_input_defaults(
+            Mission.Landing.BRAKING_DELAY, val=1.0, units="s")
+        prob.setup(check=False, force_alloc_complex=True)
+
+        partial_data = prob.check_partials(out_stream=None, method="cs")
+        assert_check_partials(partial_data, atol=2e-11, rtol=1e-12)
+
+
+class GroundRollTestCase(unittest.TestCase):
+    def setUp(self):
+
+        self.prob = om.Problem()
+        self.prob.model.add_subsystem(
+            "group", LandingGroundRollComponent(), promotes=["*"]
+        )
+
+        self.prob.model.set_input_defaults("touchdown_CD", val=0.07344)
+        self.prob.model.set_input_defaults("touchdown_CL", val=1.18694)
+        self.prob.model.set_input_defaults(
+            Mission.Landing.STALL_VELOCITY, val=109.73, units="kn"
+        )  # note: EAS in GASP, although at this altitude they are nearly identical
+        self.prob.model.set_input_defaults("TAS_touchdown", val=126.27, units="kn")
+        self.prob.model.set_input_defaults("thrust_idle", val=1276, units="lbf")
+        self.prob.model.set_input_defaults(
+            "density_ratio", val=0.998739, units="unitless"
+        )  # note: calculated from GASP glide speed values
+        self.prob.model.set_input_defaults(
+            "wing_loading_land", val=120.61, units="lbf/ft**2"
+        )
+        self.prob.model.set_input_defaults("glide_distance", val=802, units="ft")
+        self.prob.model.set_input_defaults("tr_distance", val=167, units="ft")
+        self.prob.model.set_input_defaults("delay_distance", val=213, units="ft")
+        self.prob.model.set_input_defaults("CL_max", 2.9533, units="unitless")
+        self.prob.model.set_input_defaults("mass", 165279, units="lbm")
+
+        self.prob.setup(check=False, force_alloc_complex=True)
+
+    def test_case1(self):
+        tol = 1e-6
+        self.prob.run_model()
+
+        assert_near_equal(
+            self.prob["ground_roll_distance"], 2406.43116212, tol
+        )  # actual GASP value is: 1798
+        assert_near_equal(
+            self.prob[Mission.Landing.GROUND_DISTANCE], 3588.43116212, tol
+        )  # actual GASP value is: 2980
+        assert_near_equal(
+            self.prob["average_acceleration"], 0.29308129, tol
+        )  # actual GASP value is: 0.3932
+
+        partial_data = self.prob.check_partials(out_stream=None, method="cs")
+        assert_check_partials(partial_data, atol=5e-12, rtol=1e-12)
+
+
+class GroundRollTestCase2(unittest.TestCase):
+    """
+    Test mass-weight conversion
+    """
+
+    def setUp(self):
+        import aviary.mission.gasp_based.phases.landing_components as landing
+        landing.GRAV_ENGLISH_LBM = 1.1
+
+    def tearDown(self):
+        import aviary.mission.gasp_based.phases.landing_components as landing
+        landing.GRAV_ENGLISH_LBM = 1.0
+
+    def test_case1(self):
+        self.prob = om.Problem()
+        self.prob.model.add_subsystem(
+            "group", LandingGroundRollComponent(), promotes=["*"]
+        )
+
+        self.prob.model.set_input_defaults("touchdown_CD", val=0.07344)
+        self.prob.model.set_input_defaults("touchdown_CL", val=1.18694)
+        self.prob.model.set_input_defaults(
+            Mission.Landing.STALL_VELOCITY, val=109.73, units="kn"
+        )  # note: EAS in GASP, although at this altitude they are nearly identical
+        self.prob.model.set_input_defaults("TAS_touchdown", val=126.27, units="kn")
+        self.prob.model.set_input_defaults("thrust_idle", val=1276, units="lbf")
+        self.prob.model.set_input_defaults(
+            "density_ratio", val=0.998739, units="unitless"
+        )  # note: calculated from GASP glide speed values
+        self.prob.model.set_input_defaults(
+            "wing_loading_land", val=120.61, units="lbf/ft**2"
+        )
+        self.prob.model.set_input_defaults("glide_distance", val=802, units="ft")
+        self.prob.model.set_input_defaults("tr_distance", val=167, units="ft")
+        self.prob.model.set_input_defaults("delay_distance", val=213, units="ft")
+        self.prob.model.set_input_defaults("CL_max", 2.9533, units="unitless")
+        self.prob.model.set_input_defaults("mass", 165279, units="lbm")
+
+        self.prob.setup(check=False, force_alloc_complex=True)
+
+        self.prob.run_model()
+
+        partial_data = self.prob.check_partials(out_stream=None, method="cs")
+        assert_check_partials(partial_data, atol=5e-12, rtol=1e-12)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ aviary/mission/gasp_based/ode/test/test_landing_ode.py
@@ -0,0 +1,72 @@
+from packaging import version
+import unittest
+
+import numpy as np
+
+import openmdao
+import openmdao.api as om
+from openmdao.utils.assert_utils import assert_check_partials
+
+from aviary.mission.gasp_based.ode.params import set_params_for_unit_tests
+from aviary.mission.gasp_based.ode.landing_ode import LandingSegment
+from aviary.subsystems.propulsion.utils import build_engine_deck
+from aviary.utils.test_utils.default_subsystems import get_default_mission_subsystems
+from aviary.utils.test_utils.IO_test_util import check_prob_outputs
+from aviary.variable_info.options import get_option_defaults
+from aviary.variable_info.variables import Dynamic, Mission
+
+
+class DLandTestCase(unittest.TestCase):
+    def setUp(self):
+
+        self.prob = om.Problem()
+
+        options = get_option_defaults()
+        engine = build_engine_deck(options)
+        core_subsystems = get_default_mission_subsystems('GASP', engine)
+
+        self.prob.model = LandingSegment(
+            aviary_options=options, core_subsystems=core_subsystems)
+
+    @unittest.skipIf(version.parse(openmdao.__version__) < version.parse("3.26"), "Skipping due to OpenMDAO version being too low (<3.26)")
+    def test_dland(self):
+        self.prob.setup(check=False, force_alloc_complex=True)
+
+        set_params_for_unit_tests(self.prob)
+
+        self.prob.set_val(Mission.Landing.AIRPORT_ALTITUDE, 0, units="ft")
+        self.prob.set_val(Mission.Landing.INITIAL_MACH, 0.1, units="unitless")
+        self.prob.set_val("alpha", 0, units="deg")  # doesn't matter
+        self.prob.set_val(Mission.Landing.MAXIMUM_SINK_RATE, 900, units="ft/min")
+        self.prob.set_val(Mission.Landing.GLIDE_TO_STALL_RATIO, 1.3, units="unitless")
+        self.prob.set_val(Mission.Landing.MAXIMUM_FLARE_LOAD_FACTOR,
+                          1.15, units="unitless")
+        self.prob.set_val(Mission.Landing.TOUCHDOWN_SINK_RATE, 5, units="ft/s")
+        self.prob.set_val(Mission.Landing.BRAKING_DELAY, 1, units="s")
+        self.prob.set_val("mass", 165279, units="lbm")
+        self.prob.set_val(Dynamic.Mission.THROTTLE, 0.0, units='unitless')
+
+        self.prob.run_model()
+
+        testvals = {
+            Mission.Landing.INITIAL_VELOCITY: 240.9179994,  # ft/s (142.74 knot)
+            "TAS_touchdown": 213.1197687,  # ft/s (126.27 knot)
+            "theta": 0.06230825,  # rad (3.57 deg)
+            "flare_alt": 20.8,
+            "ground_roll_distance": 1798,
+            Mission.Landing.GROUND_DISTANCE: 2980,
+            "CL_max": 2.9533,
+        }
+        check_prob_outputs(self.prob, testvals, rtol=1e-2)
+
+        partial_data = self.prob.check_partials(
+            out_stream=None, method="cs", excludes=["*params*", "*aero*"]
+        )
+        assert_check_partials(partial_data, atol=1e-6, rtol=1e-6)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    # test = DLandTestCase()
+    # test.setUp()
+    # test.test_dland()
