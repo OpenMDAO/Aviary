188a51d971dd349f889b045bb4af7e68f963bc23
Xun Jiang <xun.j.jiang@nasa.gov>
2024-10-07 08:36:25 -0700

    autopep8

--- aviary/mission/gasp_based/ode/test/test_landing_eom.py
+++ aviary/mission/gasp_based/ode/test/test_landing_eom.py
@@ -64,11 +64,14 @@ class GlideTestCase(unittest.TestCase):
         self.prob.model.set_input_defaults(
             Mission.Landing.MAXIMUM_SINK_RATE, np.ones(num_nodes)*900, units="ft/min")
 
-        self.prob.model.set_input_defaults("mass", np.ones(num_nodes)*165279, units="lbm")
-        self.prob.model.set_input_defaults(Aircraft.Wing.AREA, np.ones(num_nodes)*1370.3, units="ft**2")
+        self.prob.model.set_input_defaults(
+            "mass", np.ones(num_nodes)*165279, units="lbm")
+        self.prob.model.set_input_defaults(
+            Aircraft.Wing.AREA, np.ones(num_nodes)*1370.3, units="ft**2")
         self.prob.model.set_input_defaults(
             Mission.Landing.GLIDE_TO_STALL_RATIO, np.ones(num_nodes)*1.3, units="unitless")
-        self.prob.model.set_input_defaults("CL_max", np.ones(num_nodes)*2.9533, units="unitless")
+        self.prob.model.set_input_defaults(
+            "CL_max", np.ones(num_nodes)*2.9533, units="unitless")
         self.prob.model.set_input_defaults(
             Mission.Landing.MAXIMUM_FLARE_LOAD_FACTOR, np.ones(num_nodes)*1.15, units="unitless"
         )
@@ -94,30 +97,30 @@ class GlideTestCase(unittest.TestCase):
             self.prob.get_val(Mission.Landing.STALL_VELOCITY,
                               units="kn"), np.ones(num_nodes)*109.8331, tol
         )  # note: EAS in GASP, although at this altitude they are nearly identical. actual GASP value is 109.73
-        assert_near_equal(
-            self.prob.get_val("TAS_touchdown", units="kn"), np.ones(num_nodes)*126.3081, tol
-        )  # note: actual GASP value is: 126.27
-        assert_near_equal(
-            self.prob.get_val("density_ratio", units="unitless"), np.ones(num_nodes)*1.0, tol
-        )  # note: calculated from GASP glide speed values as: .998739
-        assert_near_equal(
-            self.prob.get_val("wing_loading_land", units="lbf/ft**2"), np.ones(num_nodes)*120.61519375, tol
-        )  # note: actual GASP value is: 120.61
-        assert_near_equal(
-            self.prob.get_val("theta", units="deg"), np.ones(num_nodes)*3.56857, tol
-        )  # note: actual GASP value is: 3.57
-        assert_near_equal(
-            self.prob.get_val("glide_distance", units="ft"), np.ones(num_nodes)*801.7444, tol
-        )  # note: actual GASP value is: 802
-        assert_near_equal(
-            self.prob.get_val("tr_distance", units="ft"), np.ones(num_nodes)*166.5303, tol
-        )  # note: actual GASP value is: 167
-        assert_near_equal(
-            self.prob.get_val("delay_distance", units="ft"), np.ones(num_nodes)*213.184, tol
-        )  # note: actual GASP value is: 213
-        assert_near_equal(
-            self.prob.get_val("flare_alt", units="ft"), np.ones(num_nodes)*20.73407, tol
-        )  # note: actual GASP value is: 20.8
+        assert_near_equal(self.prob.get_val("TAS_touchdown", units="kn"),
+                          np.ones(num_nodes)*126.3081, tol
+                          )  # note: actual GASP value is: 126.27
+        assert_near_equal(self.prob.get_val("density_ratio", units="unitless"),
+                          np.ones(num_nodes)*1.0, tol
+                          )  # note: calculated from GASP glide speed values as: .998739
+        assert_near_equal(self.prob.get_val("wing_loading_land", units="lbf/ft**2"),
+                          np.ones(num_nodes)*120.61519375, tol
+                          )  # note: actual GASP value is: 120.61
+        assert_near_equal(self.prob.get_val("theta", units="deg"),
+                          np.ones(num_nodes)*3.56857, tol
+                          )  # note: actual GASP value is: 3.57
+        assert_near_equal(self.prob.get_val("glide_distance", units="ft"),
+                          np.ones(num_nodes)*801.7444, tol
+                          )  # note: actual GASP value is: 802
+        assert_near_equal(self.prob.get_val("tr_distance", units="ft"),
+                          np.ones(num_nodes)*166.5303, tol
+                          )  # note: actual GASP value is: 167
+        assert_near_equal(self.prob.get_val("delay_distance", units="ft"),
+                          np.ones(num_nodes)*213.184, tol
+                          )  # note: actual GASP value is: 213
+        assert_near_equal(self.prob.get_val("flare_alt", units="ft"),
+                          np.ones(num_nodes)*20.73407, tol
+                          )  # note: actual GASP value is: 20.8
 
         partial_data = self.prob.check_partials(out_stream=None, method="cs")
         assert_check_partials(partial_data, atol=1e-10, rtol=1e-12)
@@ -177,24 +180,33 @@ class GroundRollTestCase(unittest.TestCase):
             "group", LandingGroundRollComponent(num_nodes=num_nodes), promotes=["*"]
         )
 
-        self.prob.model.set_input_defaults("touchdown_CD", val=np.ones(num_nodes)*0.07344)
-        self.prob.model.set_input_defaults("touchdown_CL", val=np.ones(num_nodes)*1.18694)
+        self.prob.model.set_input_defaults(
+            "touchdown_CD", val=np.ones(num_nodes)*0.07344)
+        self.prob.model.set_input_defaults(
+            "touchdown_CL", val=np.ones(num_nodes)*1.18694)
         self.prob.model.set_input_defaults(
             Mission.Landing.STALL_VELOCITY, val=np.ones(num_nodes)*109.73, units="kn"
         )  # note: EAS in GASP, although at this altitude they are nearly identical
-        self.prob.model.set_input_defaults("TAS_touchdown", val=np.ones(num_nodes)*126.27, units="kn")
-        self.prob.model.set_input_defaults("thrust_idle", val=np.ones(num_nodes)*1276, units="lbf")
+        self.prob.model.set_input_defaults(
+            "TAS_touchdown", val=np.ones(num_nodes)*126.27, units="kn")
+        self.prob.model.set_input_defaults(
+            "thrust_idle", val=np.ones(num_nodes)*1276, units="lbf")
         self.prob.model.set_input_defaults(
             "density_ratio", val=np.ones(num_nodes)*0.998739, units="unitless"
         )  # note: calculated from GASP glide speed values
         self.prob.model.set_input_defaults(
             "wing_loading_land", val=np.ones(num_nodes)*120.61, units="lbf/ft**2"
         )
-        self.prob.model.set_input_defaults("glide_distance", val=np.ones(num_nodes)*802, units="ft")
-        self.prob.model.set_input_defaults("tr_distance", val=np.ones(num_nodes)*167, units="ft")
-        self.prob.model.set_input_defaults("delay_distance", val=np.ones(num_nodes)*213, units="ft")
-        self.prob.model.set_input_defaults("CL_max", np.ones(num_nodes)*2.9533, units="unitless")
-        self.prob.model.set_input_defaults("mass", np.ones(num_nodes)*165279, units="lbm")
+        self.prob.model.set_input_defaults(
+            "glide_distance", val=np.ones(num_nodes)*802, units="ft")
+        self.prob.model.set_input_defaults(
+            "tr_distance", val=np.ones(num_nodes)*167, units="ft")
+        self.prob.model.set_input_defaults(
+            "delay_distance", val=np.ones(num_nodes)*213, units="ft")
+        self.prob.model.set_input_defaults(
+            "CL_max", np.ones(num_nodes)*2.9533, units="unitless")
+        self.prob.model.set_input_defaults(
+            "mass", np.ones(num_nodes)*165279, units="lbm")
 
         self.prob.setup(check=False, force_alloc_complex=True)
 
@@ -206,9 +218,9 @@ class GroundRollTestCase(unittest.TestCase):
         assert_near_equal(
             self.prob["ground_roll_distance"], np.ones(num_nodes)*2406.43116212, tol
         )  # actual GASP value is: 1798
-        assert_near_equal(
-            self.prob[Mission.Landing.GROUND_DISTANCE], np.ones(num_nodes)*3588.43116212, tol
-        )  # actual GASP value is: 2980
+        assert_near_equal(self.prob[Mission.Landing.GROUND_DISTANCE],
+                          np.ones(num_nodes)*3588.43116212, tol
+                          )  # actual GASP value is: 2980
         assert_near_equal(
             self.prob["average_acceleration"], np.ones(num_nodes)*0.29308129, tol
         )  # actual GASP value is: 0.3932
