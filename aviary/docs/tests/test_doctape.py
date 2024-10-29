66c3e7c8a64e2789966768a4101a3aad06d27ca4
Carl Recine <carl.recine@nasa.gov>
2024-10-29 12:09:14 -0700

    splitting DocTAPE examples and code docs

---
+++ aviary/docs/tests/test_doctape.py
@@ -0,0 +1,48 @@
+import unittest
+
+import aviary.docs.tests.utils as doctape
+
+
+class DocTAPETests(unittest.TestCase):
+    """
+    Testing the DocTAPE functions to make sure they all run in all supported Python versions
+    Docs are only built with latest, but these will be run with latest and dev as well
+    """
+
+    def test_gramatical_list(self):
+        doctape.gramatical_list(['a', 'b', 'c'])
+
+    def test_check_value(self):
+        doctape.check_value(1, 1.0)
+
+    def test_check_contains(self):
+        doctape.check_contains(1, [1, 2, 3])
+
+    def test_check_args(self):
+        doctape.check_args(doctape.check_args, 'func')
+
+    def test_run_command_no_file_error(self):
+        doctape.run_command_no_file_error('python -c "print()"')
+
+    def test_get_attribute_name(self):
+        class dummy_object:
+            attr1 = 1
+        doctape.get_attribute_name(dummy_object, 1)
+
+    def test_get_all_keys(self):
+        doctape.get_all_keys({'d1': {'d2': 2}})
+
+    def test_get_value(self):
+        doctape.get_value({'d1': {'d2': 2}}, 'd1.d2')
+
+    # requires IPython shell
+    # def test_glue_variable(self):
+    #     doctape.glue_variable('plain_text')
+
+    # requires IPython shell
+    # def test_glue_keys(self):
+    #     doctape.glue_keys({'d1':{'d2':2}})
+
+
+if __name__ == '__main__':
+    unittest.main()
