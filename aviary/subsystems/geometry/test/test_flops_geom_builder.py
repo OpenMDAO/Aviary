611e7ae918ea786e03f12c6fa56d833ee91bb498
Xun Jiang <xjjiang@users.noreply.github.com>
2024-11-12 17:05:57 -0800

    Update aviary/subsystems/geometry/test/test_flops_geom_builder.py
    Set use+both+geometries=False and add code_origin=FLOPS. Give it a try.
    
    Co-authored-by: crecine <51181861+crecine@users.noreply.github.com>

--- aviary/subsystems/geometry/test/test_flops_geom_builder.py
+++ aviary/subsystems/geometry/test/test_flops_geom_builder.py
@@ -25,7 +25,8 @@ class TestFLOPSGeomBuilder(av.TestSubsystemBuilderBase):
         self.subsystem_builder = CoreGeometryBuilder(
             'core_geometry',
             BaseMetaData,
-            use_both_geometries=True,
+            use_both_geometries=False,
+            code_origin=FLOPS,
             code_origin_to_prioritize=FLOPS)
         self.aviary_values = av.AviaryValues()
         self.aviary_values.set_val(Aircraft.Engine.NUM_ENGINES, [1], units='unitless')
