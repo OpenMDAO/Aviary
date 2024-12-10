f17ca3eb0f009550eb72ebbdced74c982629b1e8
jkirk5 <jkirk5@grayskull.larc.nasa.gov>
2024-12-10 17:28:33 -0500

    fixed other conversion tests

--- aviary/utils/test/test_fortran_to_aviary.py
+++ aviary/utils/test/test_fortran_to_aviary.py
@@ -17,9 +17,6 @@ class TestFortranToAviary(unittest.TestCase):
     """
 
     def prepare_and_run(self, filepath, out_file=None, legacy_code=LegacyCode.GASP):
-        # Specify the input file and the legacy code
-        input_deck = filepath
-
         # Specify the output file
         filename = filepath.split('.')[0]+'.csv'
         if not out_file:
@@ -71,16 +68,22 @@ class TestFortranToAviary(unittest.TestCase):
             'utils/test/data/converter_test_data_large_single_aisle_1_GASP.csv'
         )
 
-        self.prepare_and_run(filepath)
+        self.prepare_and_run(
+            filepath,
+            out_file=Path.cwd() / Path('TEST_' + comparison_filepath),
+        )
         self.compare_files(comparison_filepath)
 
     def test_small_single_aisle(self):
-        filepath = 'models/small_single_aisle_1/small_single_aisle_1_GASP.dat'
+        filepath = 'models/small_single_aisle/small_single_aisle_GASP.dat'
         comparison_filepath = (
-            'utils/test/data/converter_test_data_small_single_aisle_1_GASP.csv'
+            'utils/test/data/converter_test_data_small_single_aisle_GASP.csv'
         )
 
-        self.prepare_and_run(filepath)
+        self.prepare_and_run(
+            filepath,
+            out_file=Path.cwd() / Path('TEST_' + comparison_filepath),
+        )
         self.compare_files(comparison_filepath)
 
     def test_diff_configuration(self):
@@ -95,15 +98,15 @@ class TestFortranToAviary(unittest.TestCase):
         # and was not evaluated for comparison to the original. Thus, until this file is evaluated, this test is purely a regression
         # test.
 
-        filepath = 'models/N3CC/N3CC_generic_low_speed_polars_FLOPSinp.txt'
+        filepath = 'models/N3CC/N3CC_generic_low_speed_polars_FLOPS.txt'
         comparison_filepath = 'utils/test/data/converter_test_data_N3CC_FLOPS.csv'
-
-        self.prepare_and_run(filepath, legacy_code=LegacyCode.FLOPS)
+        self.prepare_and_run(
+            filepath,
+            out_file=Path.cwd() / Path('TEST_' + comparison_filepath),
+            legacy_code=LegacyCode.FLOPS,
+        )
         self.compare_files(comparison_filepath)
 
 
 if __name__ == "__main__":
-    # unittest.main()
-
-    test = TestFortranToAviary()
-    test.test_diff_configuration()
+    unittest.main()
