944930ef4d6c86dab5870567faf10eacdcbb7f64
Xun Jiang <xun.j.jiang@nasa.gov>
2025-01-02 11:25:19 -0800

    Changed Aircraft.Fuselage.LENGTH from 129.4 to 0.0. Changed Aircraft.Wing.AREA from 1370 to 0.0.

--- aviary/subsystems/geometry/gasp_based/empennage.py
+++ aviary/subsystems/geometry/gasp_based/empennage.py
@@ -38,23 +38,19 @@ class TailVolCoef(om.ExplicitComponent):
         else:
             self.k = [0.43, 0.38, 0.85]
 
-        add_aviary_input(self, Aircraft.HorizontalTail.VERTICAL_TAIL_FRACTION, val=0)
+        add_aviary_input(self, Aircraft.HorizontalTail.VERTICAL_TAIL_FRACTION)
 
-        add_aviary_input(self, Aircraft.Fuselage.LENGTH, val=129.4)
+        add_aviary_input(self, Aircraft.Fuselage.LENGTH)
 
         self.add_input("cab_w", 13.1, units="ft", desc="SWF: Cabin width")
 
-        add_aviary_input(self, Aircraft.Wing.AREA, val=1370)
+        add_aviary_input(self, Aircraft.Wing.AREA)
 
-        self.add_input(
-            "wing_ref",
-            12.615,
-            units="ft",
-            desc=(
-                "CBARW | B: Wing reference parameter. Wing chord for a "
-                "horizontal tail. Wing span for a vertical tail."
-            ),
-        )
+        self.add_input("wing_ref", 12.615, units="ft",
+                       desc=(
+                           "CBARW | B: Wing reference parameter. Wing chord for a "
+                           "horizontal tail. Wing span for a vertical tail.")
+                       )
 
         self.add_output(
             "vol_coef", units="unitless", desc="VBARH | VBARV: Tail volume coefficient")
