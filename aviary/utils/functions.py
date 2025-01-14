22f2651ede487146de6fe38a8f93c747bb7205a7
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2025-01-14 18:03:10 -0500

    Reworked a bit, since I forgot the configure method was being used to specify promtoes.

--- aviary/utils/functions.py
+++ aviary/utils/functions.py
@@ -361,6 +361,7 @@ def promote_aircraft_and_mission_vars(group):
                     break
 
         group.promotes(comp.name, outputs=promote_out, inputs=promote_in)
+        print(comp.name, promote_in, promote_out)
 
     return external_outputs
 
