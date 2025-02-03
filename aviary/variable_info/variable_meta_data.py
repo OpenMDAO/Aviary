Fix up `total_thrust` calculation in `process_input_decks.initialization_guessing`

aviary/variable_info/variable_meta_data.py
@@ -2101,7 +2101,7 @@ add_meta_data(
     types=(np.ndarray, int),
     multivalue=True,
     option=True,
-    default_value=2
+    default_value=[2]
 )
 
 add_meta_data(