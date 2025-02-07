0bfb8a66da80811cc9b24fbc229c8302094679ce
Xun Jiang <xun.j.jiang@nasa.gov>
2025-02-06 20:34:17 -0800

    work in progress for Cl_max.py

--- aviary/subsystems/aerodynamics/gasp_based/flaps_model/Cl_max.py
+++ aviary/subsystems/aerodynamics/gasp_based/flaps_model/Cl_max.py
@@ -120,10 +120,9 @@ class CLmaxCalculation(om.ExplicitComponent):
             units='unitless',
             desc="DELCLF: fuselage lift increment",
         )
-        add_aviary_input(self, Dynamic.Atmosphere.KINEMATIC_VISCOSITY, val=0.15723e-03,
-                         units="ft**2/s", desc="XKV: kinematic viscosity")
-        add_aviary_input(self, Dynamic.Atmosphere.TEMPERATURE, val=518.67,
-                         units="degR", desc="T0: static temperature of air cross wing")
+        add_aviary_input(self, Dynamic.Atmosphere.KINEMATIC_VISCOSITY, desc="INGASP.XKV")
+        add_aviary_input(self, Dynamic.Atmosphere.TEMPERATURE,
+                         units="degR", desc="INGASP.T0")
 
         # outputs
 
@@ -133,8 +132,7 @@ class CLmaxCalculation(om.ExplicitComponent):
             desc="CLMAX: maximum lift coefficient",
             units="unitless",
         )
-        add_aviary_output(self, Dynamic.Atmosphere.MACH, val=0.17522,
-                          units='unitless', desc="SMN: mach number")
+        add_aviary_output(self, Dynamic.Atmosphere.MACH, desc="INGASP.SMN")
         self.add_output(
             "reynolds", val=157.1111, units='unitless', desc="RNW: reynolds number"
         )
