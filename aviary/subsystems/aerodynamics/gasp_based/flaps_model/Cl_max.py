9e44621812270134aeff2b83777cae957988b19e
Xun Jiang <xun.j.jiang@nasa.gov>
2025-02-09 15:24:56 -0800

    set Dynamic.Atmosphere.SPEED_OF_SOUND, Aircraft.Wing.FLAP_LIFT_INCREMENT_OPTIMUM, Aircraft.Wing.LOADING to defaults in Cl_max.py

--- aviary/subsystems/aerodynamics/gasp_based/flaps_model/Cl_max.py
+++ aviary/subsystems/aerodynamics/gasp_based/flaps_model/Cl_max.py
@@ -1,6 +1,6 @@
 import openmdao.api as om
 
-from aviary.variable_info.functions import add_aviary_input
+from aviary.variable_info.functions import add_aviary_input, add_aviary_output
 from aviary.variable_info.variables import Aircraft, Dynamic
 
 
@@ -20,15 +20,10 @@ class CLmaxCalculation(om.ExplicitComponent):
             units='unitless',
             desc="VLAM8: sensitivity of flap clean wing maximum lift coefficient to wing sweep angle",
         )
-        self.add_input(
-            Dynamic.Atmosphere.SPEED_OF_SOUND,
-            val=1118.21948771,
-            units="ft/s",
-            desc="SA: speed of sound at sea level",
-        )
+        add_aviary_input(self, Dynamic.Atmosphere.SPEED_OF_SOUND, desc="INGASP.SA")
 
         # from component 3 outputs
-        add_aviary_input(self, Aircraft.Wing.FLAP_LIFT_INCREMENT_OPTIMUM, val=1.5)
+        add_aviary_input(self, Aircraft.Wing.FLAP_LIFT_INCREMENT_OPTIMUM)
         self.add_input(
             "VLAM1",
             val=0.97217,
@@ -86,7 +81,7 @@ class CLmaxCalculation(om.ExplicitComponent):
 
         # other inputs
 
-        add_aviary_input(self, Aircraft.Wing.LOADING, val=128)
+        add_aviary_input(self, Aircraft.Wing.LOADING)
 
         self.add_input(
             Dynamic.Atmosphere.STATIC_PRESSURE,
