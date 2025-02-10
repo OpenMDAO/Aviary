2b3c371077a037928eeabf4f21a9683f96126821
Xun Jiang <xun.j.jiang@nasa.gov>
2025-02-10 14:50:47 -0500

    add a motor to ElectricCruiseODETestCase

--- aviary/mission/gasp_based/ode/test/test_breguet_cruise_ode.py
+++ aviary/mission/gasp_based/ode/test/test_breguet_cruise_ode.py
@@ -9,8 +9,10 @@ from aviary.mission.gasp_based.ode.params import set_params_for_unit_tests
 from aviary.subsystems.propulsion.utils import build_engine_deck
 from aviary.utils.test_utils.default_subsystems import get_default_mission_subsystems
 from aviary.variable_info.options import get_option_defaults
-from aviary.variable_info.variables import Aircraft, Dynamic
+from aviary.variable_info.variables import Aircraft, Dynamic, Mission
 
+from aviary.subsystems.propulsion.motor.motor_builder import MotorBuilder
+from aviary.subsystems.propulsion.turboprop_model import TurbopropModel
 
 class CruiseODETestCase(unittest.TestCase):
     def setUp(self):
@@ -73,10 +75,35 @@ class ElectricCruiseODETestCase(unittest.TestCase):
     def setUp(self):
         self.prob = om.Problem()
 
+        motor_model = MotorBuilder()
+        options = get_option_defaults()
+        options.set_val(Aircraft.Engine.NUM_ENGINES, 2)
+        options.set_val(Aircraft.Engine.SUBSONIC_FUEL_FLOW_SCALER, 1.0)
+        options.set_val(Aircraft.Engine.SUPERSONIC_FUEL_FLOW_SCALER, 1.0)
+        options.set_val(Aircraft.Engine.FUEL_FLOW_SCALER_CONSTANT_TERM, 0.0)
+        options.set_val(Aircraft.Engine.FUEL_FLOW_SCALER_LINEAR_TERM, 1.0)
+        options.set_val(Aircraft.Engine.CONSTANT_FUEL_CONSUMPTION, 0.0, units='lbm/h')
+        options.set_val(Aircraft.Engine.SCALE_PERFORMANCE, True)
+        options.set_val(Mission.Summary.FUEL_FLOW_SCALER, 1.0)
+        options.set_val(Aircraft.Engine.SCALE_FACTOR, 1)
+        options.set_val(Aircraft.Engine.GENERATE_FLIGHT_IDLE, False)
+        options.set_val(Aircraft.Engine.IGNORE_NEGATIVE_THRUST, False)
+        options.set_val(Aircraft.Engine.FLIGHT_IDLE_THRUST_FRACTION, 0.0)
+        options.set_val(Aircraft.Engine.FLIGHT_IDLE_MAX_FRACTION, 1.0)
+        options.set_val(Aircraft.Engine.FLIGHT_IDLE_MIN_FRACTION, 0.08)
+        options.set_val(Aircraft.Engine.GEOPOTENTIAL_ALT, False)
+        options.set_val(Aircraft.Engine.INTERPOLATION_METHOD, 'slinear')
+        options.set_val(Aircraft.Engine.FIXED_RPM, 1455.13090827, units='rpm')
+        options.set_val(Aircraft.Engine.Propeller.COMPUTE_INSTALLATION_LOSS, val=True)
+        options.set_val(Aircraft.Engine.Propeller.NUM_BLADES, val=4, units='unitless')
+        engine = TurbopropModel(
+            options=options, shaft_power_model=motor_model, propeller_model=None
+        )
+
         aviary_options = get_option_defaults()
         aviary_options.set_val(Aircraft.Engine.GLOBAL_THROTTLE, True)
         default_mission_subsystems = get_default_mission_subsystems(
-            'GASP', build_engine_deck(aviary_options)
+            'GASP', engine
         )
 
         self.prob.model = E_BreguetCruiseODESolution(
@@ -92,6 +119,17 @@ class ElectricCruiseODETestCase(unittest.TestCase):
     def test_cruise(self):
         # test partial derivatives
         self.prob.setup(check=False, force_alloc_complex=True)
+
+        self.prob.set_val(Dynamic.Vehicle.Propulsion.RPM,
+                          np.ones(2) * 2000.0, units='rpm')
+        self.prob.set_val(Aircraft.Engine.Propeller.DIAMETER, 10.5, units="ft")
+        self.prob.set_val(
+            Aircraft.Engine.Propeller.ACTIVITY_FACTOR, 114.0, units="unitless"
+        )
+        self.prob.set_val(
+            Aircraft.Engine.Propeller.INTEGRATED_LIFT_COEFFICIENT, 0.5, units="unitless"
+        )
+        self.prob.set_val(Aircraft.Engine.Propeller.TIP_SPEED_MAX, 800, units="ft/s")
 
         self.prob.set_val(Dynamic.Atmosphere.MACH, [0.7, 0.7], units="unitless")
         self.prob.set_val("interference_independent_of_shielded_area", 1.89927266)
