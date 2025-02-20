3ba95a2f74fabaf82398534d0018d9510c3f8583
Xun Jiang <xun.j.jiang@nasa.gov>
2025-02-19 17:12:49 -0800

    add Aircraft.VerticalTail.FORM_FACTOR = 1.25, Aircraft.HorizontalTail.FORM_FACTOR = 1.25.

--- aviary/mission/gasp_based/ode/test/test_climb_ode.py
+++ aviary/mission/gasp_based/ode/test/test_climb_ode.py
@@ -56,6 +56,8 @@ class ClimbODETestCase(unittest.TestCase):
         self.prob.set_val("interference_independent_of_shielded_area", 1.89927266)
         self.prob.set_val("drag_loss_due_to_shielded_wing_area", 68.02065834)
         self.prob.set_val(Aircraft.Wing.FORM_FACTOR, 1.25)
+        self.prob.set_val(Aircraft.VerticalTail.FORM_FACTOR, 1.25)
+        self.prob.set_val(Aircraft.HorizontalTail.FORM_FACTOR, 1.25)
 
         set_params_for_unit_tests(self.prob)
 
@@ -101,6 +103,8 @@ class ClimbODETestCase(unittest.TestCase):
         self.prob.set_val("interference_independent_of_shielded_area", 1.89927266)
         self.prob.set_val("drag_loss_due_to_shielded_wing_area", 68.02065834)
         self.prob.set_val(Aircraft.Wing.FORM_FACTOR, 1.25)
+        self.prob.set_val(Aircraft.VerticalTail.FORM_FACTOR, 1.25)
+        self.prob.set_val(Aircraft.HorizontalTail.FORM_FACTOR, 1.25)
 
         set_params_for_unit_tests(self.prob)
 
--- aviary/mission/gasp_based/ode/test/test_groundroll_ode.py
+++ aviary/mission/gasp_based/ode/test/test_groundroll_ode.py
@@ -44,6 +44,8 @@ class GroundrollODETestCase(unittest.TestCase):
         self.prob.set_val("interference_independent_of_shielded_area", 1.89927266)
         self.prob.set_val("drag_loss_due_to_shielded_wing_area", 68.02065834)
         self.prob.set_val(Aircraft.Wing.FORM_FACTOR, 1.25)
+        self.prob.set_val(Aircraft.VerticalTail.FORM_FACTOR, 1.25)
+        self.prob.set_val(Aircraft.HorizontalTail.FORM_FACTOR, 1.25)
 
         self.prob.run_model()
 
