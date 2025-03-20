f29c27a6557bf924d98ef8833882870155a31ee0
Xun Jiang <xjjiang@users.noreply.github.com>
2025-03-19 17:30:06 -0700

    Update variable_meta_data.py
    renamed a variable.

--- aviary/variable_info/variable_meta_data.py
+++ aviary/variable_info/variable_meta_data.py
@@ -4280,15 +4280,6 @@ add_meta_data(
 )
 
 add_meta_data(
-    Aircraft.Nacelle.PERCENT_DIAMETER_BURIED_IN_FUSELAGE,
-    meta_data=_MetaData,
-    historical_name={"GASP": 'INGASP.HEBQDN', "FLOPS": None, "LEAPS1": None},
-    units='unitless',
-    default_value=0.0,
-    desc='percentage of nacelle diameter buried in fuselage over nacelle diameter',
-)
-
-add_meta_data(
     Aircraft.Nacelle.FINENESS,
     meta_data=_MetaData,
     historical_name={
@@ -4378,6 +4369,15 @@ add_meta_data(
     units='lbm/ft**2',
     desc='nacelle mass/nacelle surface area; lbm per sq ft.',
     default_value=0.0,
+)
+
+add_meta_data(
+    Aircraft.Nacelle.PERCENT_DIAMETER_BURIED_IN_FUSELAGE,
+    meta_data=_MetaData,
+    historical_name={"GASP": 'INGASP.HEBQDN', "FLOPS": None, "LEAPS1": None},
+    units='unitless',
+    default_value=0.0,
+    desc='percentage of nacelle diameter buried in fuselage over nacelle diameter',
 )
 
 add_meta_data(
