3684f6a3729923c98dceab9f9ba003cc431f50b1
Xun Jiang <xun.j.jiang@nasa.gov>
2025-03-24 16:01:47 -0400

    add BWBCabinLayout

--- aviary/subsystems/geometry/gasp_based/fuselage.py
+++ aviary/subsystems/geometry/gasp_based/fuselage.py
@@ -256,3 +256,202 @@ class FuselageGroup(om.Group):
             promotes_inputs=connected_input_outputs + ["aircraft:*"],
             promotes_outputs=["aircraft:*"],
         )
+
+
+class BWBFuselageParameters(om.ExplicitComponent):
+    """
+    Computation of average fuselage diameter, cabin height, cabin length and nose height for BWB.
+    """
+
+    def initialize(self):
+        add_aviary_option(self, Aircraft.CrewPayload.Design.NUM_PASSENGERS)
+        add_aviary_option(self, Aircraft.Fuselage.AISLE_WIDTH, units='inch')
+        add_aviary_option(self, Aircraft.Fuselage.NUM_AISLES)
+        add_aviary_option(self, Aircraft.Fuselage.NUM_SEATS_ABREAST)
+        add_aviary_option(self, Aircraft.Fuselage.SEAT_PITCH, units='inch')
+        add_aviary_option(self, Aircraft.Fuselage.SEAT_WIDTH, units='inch')
+        add_aviary_option(self, Settings.VERBOSITY)
+
+    def setup(self):
+        pass
+
+
+class BWBCabinLayout(om.ExplicitComponent):
+    """
+    layout of passenger cabin for BWB
+    """
+
+    def initialize(self):
+        add_aviary_option(self, Aircraft.Fuselage.NUM_SEATS_ABREAST,
+                          units='unitless', desc='INGASP.SAB')
+        add_aviary_option(self, Aircraft.Fuselage.SEAT_WIDTH,
+                          units='inch', desc='INGASP.WS')
+        add_aviary_option(self, Aircraft.Fuselage.NUM_AISLES,
+                          units='unitless', desc='INGASP.AS')
+        add_aviary_option(self, Aircraft.Fuselage.AISLE_WIDTH,
+                          units='inch', desc='INGASP.WAS')
+        add_aviary_option(self, Aircraft.Fuselage.SEAT_PITCH,
+                          units='inch', desc='INGASP.PS')
+        add_aviary_option(self, Aircraft.CrewPayload.Design.NUM_PASSENGERS,
+                          units='unitless', desc='INGASP.PAX')
+        add_aviary_option(self, Aircraft.CrewPayload.Design.NUM_FIRST_CLASS,
+                          units='unitless', desc='equiv INGASP.PCT_FC')
+        add_aviary_option(self, Settings.VERBOSITY)
+
+    def setup(self):
+        add_aviary_input(self, Aircraft.BWB.PASSENGER_LEADING_EDGE_SWEEP,
+                         units='deg')
+        add_aviary_input(self, Aircraft.Fuselage.PILOT_COMPARTMENT_LENGTH,
+                         units='ft')
+        self.add_input('nose_length', units='ft')
+
+        self.add_output('fuselage_station_aft', units='ft',
+                        desc='EL_AFT: fuselage station of aft pressure bulkhead')
+
+        self.declare_partials('*', '*', method='fd', form='forward')
+
+    def compute(self, inputs, outputs):
+        options = self.options
+        verbosity = options[Settings.VERBOSITY]
+        rad2deg = 180. / np.pi
+
+        # Hard code variables in GASP:
+        FC_lav_galley_length = 8.0  # EL_FLGC: length of first class lav, galley & closet, ft
+        FC_seat_width = 28.0  # WS_FC: first class seat width, inch
+        FC_seat_pitch = 36.0  # PS_FC: first class seat pitch, inch
+        FC_num_aisles = 2  # AS_FC: num of aisles in first class
+        FC_aisle_width = 24.0  # WAS_FC: First class aisle width, inch
+        length_FC_to_TC = 5.0  # Length of first class/tourist class aisle
+        TC_num_pax_per_lav = 78  # NLAVTC: tourist class passengers per lav
+        TC_lav_width = 42.0  # WIDLAV: Lav width, inches
+        TC_galley_area_per_pax = 0.15  # AGAL_TC: tourist class galley area per passenger, ft**2
+        # If there is no first class cabin, please set NUM_FIRST_CLASS = 0.
+
+        num_seat_abreast = options[Aircraft.Fuselage.NUM_SEATS_ABREAST]
+        TC_seat_pitch, _ = options[Aircraft.Fuselage.SEAT_PITCH]
+        seat_width, _ = options[Aircraft.Fuselage.SEAT_WIDTH]
+        if seat_width <= 0.0:
+            raise ValueError('fuselage seat width must be positive.')
+        aisle_width, _ = options[Aircraft.Fuselage.AISLE_WIDTH]
+        num_aisles = options[Aircraft.Fuselage.NUM_AISLES]
+
+        cabin_width = (num_seat_abreast * seat_width +
+                       num_aisles * aisle_width) / 12.0 + 1.
+
+        sweep_FB = inputs[Aircraft.BWB.PASSENGER_LEADING_EDGE_SWEEP][0]
+        pax = options[Aircraft.CrewPayload.Design.NUM_PASSENGERS]
+        pax_FC = options[Aircraft.CrewPayload.Design.NUM_FIRST_CLASS]
+        if pax_FC <= 0:
+            if verbosity > Verbosity.BRIEF:
+                print(
+                    "Warning: There are no first class passengers. First class cabin is not included.")
+        if pax_FC > pax:
+            raise ValueError(
+                'Number of first class passengers must not exceed the total number of passengers.')
+        pax_TC = pax - pax_FC
+
+        nose_length = inputs['nose_length'][0]
+        pilot_com_length = inputs[Aircraft.Fuselage.PILOT_COMPARTMENT_LENGTH][0]
+        if pax_FC > 0:
+            fwd_pax_fuselage_station = nose_length + pilot_com_length + FC_lav_galley_length
+        else:
+            fwd_pax_fuselage_station = nose_length + pilot_com_length
+
+        # First Class
+        length_FC_by_row = []  # length in first class, ft
+        width_FC_by_row = []  # width in first class, ft
+        num_seats_FC_by_row = []  # num of seats in first class
+        sum_num_seats_FC = 0
+        EL_FC_last_row = 0.0
+        if pax_FC > 0:
+            EL_FC_ptr = fwd_pax_fuselage_station - FC_seat_pitch / 12.0
+
+            Idx_row_FC = 0
+            while sum_num_seats_FC < pax_FC:
+                len = EL_FC_ptr + FC_seat_pitch / 12.0
+                length_FC_by_row.append(len)
+                wid = 2.0 * length_FC_by_row[Idx_row_FC] / np.tan(sweep_FB / rad2deg)
+                width_FC_by_row.append(wid)
+                if width_FC_by_row[Idx_row_FC] > cabin_width:
+                    width_FC_by_row[Idx_row_FC] = cabin_width
+                wid_aisle = FC_num_aisles * FC_aisle_width / 12.0
+                num = int(
+                    (width_FC_by_row[Idx_row_FC] - wid_aisle) / FC_seat_width * 12.0
+                )
+                num_seats_FC_by_row.append(num)
+                prev_sum_num_seats_FC = sum_num_seats_FC
+                sum_num_seats_FC = sum_num_seats_FC + num_seats_FC_by_row[Idx_row_FC]
+                if sum_num_seats_FC > pax_FC:
+                    break
+                EL_FC_ptr = length_FC_by_row[Idx_row_FC]
+                Idx_row_FC = Idx_row_FC + 1
+
+            # Last row of first class
+            EL_FC_last_row = length_FC_by_row[Idx_row_FC]
+            num_seats_last_row = pax_FC - prev_sum_num_seats_FC
+            # If only one seat in last row, delete last row & assume seat located forward
+            if num_seats_last_row < 2:
+                EL_FC_last_row = length_FC_by_row[Idx_row_FC - 1]
+                EL_FC_ptr = length_FC_by_row[Idx_row_FC - 1]
+
+            sum_num_seats_FC = pax_FC
+        else:
+            # If not first class
+            EL_FC_last_row = 0
+
+        # First Class/Tourist Class Aisle
+        if pax_FC > 0:
+            EL_TC_ptr = EL_FC_last_row + FC_seat_pitch / 12.0
+        else:
+            EL_TC_ptr = fwd_pax_fuselage_station
+
+        # Tourist Class
+        length_TC_by_row = []  # length in tourist class, ft
+        width_TC_by_row = []  # width in tourist class, ft
+        num_seats_TC_by_row = []  # num of seats in tourist class
+        sum_num_seats_TC = 0
+        if pax_FC > 0:
+            EL_TC_ptr = EL_TC_ptr + length_FC_to_TC - TC_seat_pitch / 12.0
+        else:
+            EL_TC_ptr = EL_TC_ptr - TC_seat_pitch / 12.0
+        Idx_row_TC = 0
+        while sum_num_seats_TC < pax_TC:
+            len = EL_TC_ptr + TC_seat_pitch / 12.0
+            length_TC_by_row.append(len)
+            wid = 2.0 * length_TC_by_row[Idx_row_TC] / np.tan(sweep_FB / rad2deg)
+            width_TC_by_row.append(wid)
+            if width_TC_by_row[Idx_row_TC] > cabin_width:
+                width_TC_by_row[Idx_row_TC] = cabin_width
+            width_aisle = num_aisles * aisle_width / 12.0
+            num = int(
+                (width_TC_by_row[Idx_row_TC] - width_aisle) / (seat_width / 12.0)
+            )
+            num_seats_TC_by_row.append(num)
+            prev_num_seats_TC = sum_num_seats_TC
+            sum_num_seats_TC = sum_num_seats_TC + num_seats_TC_by_row[Idx_row_TC]
+            if sum_num_seats_TC >= pax_TC:
+                break
+            EL_TC_ptr = length_TC_by_row[Idx_row_TC]
+            Idx_row_TC = Idx_row_TC + 1
+
+        sum_num_seats_TC = pax_TC
+        # last row in tourist class: find number of seats in last row
+        num_seats_last_row = sum_num_seats_TC - prev_num_seats_TC
+        # find width available for last row for lavs/galleys (Assumes Steward's seat in TC aisle)
+        width_last_row = num_seats_last_row * seat_width / 12.0
+        width_aisle = num_aisles * aisle_width / 12.0
+        wid_last_row_avail = cabin_width - width_last_row - width_aisle
+        # find number of tourist class lavs and aft galley (galley width = lav width)
+        num_lav_TC = int(sum_num_seats_TC / TC_num_pax_per_lav) + 1
+        wid_galley = 144.0 * sum_num_seats_TC * TC_galley_area_per_pax / TC_lav_width
+
+        # find EL_AFT = fuselage station of aft pressure bulkhead. ft
+        if wid_last_row_avail >= num_lav_TC * TC_lav_width / 12.0 + wid_galley / 12.0:
+            # Add lavs/gallley to last row
+            EL_AFT = length_TC_by_row[Idx_row_TC] + TC_lav_width / 12.0
+        else:
+            # add additional row for lavs & galleys to last row
+            EL_AFT = length_TC_by_row[Idx_row_TC] + \
+                TC_seat_pitch / 12.0 + TC_lav_width / 12.0
+
+        outputs['fuselage_station_aft'] = EL_AFT
