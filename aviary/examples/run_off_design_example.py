812dbeaa85bde02619d93b66bc121175818d52b2
jkirk5 <jason.kirk@nasa.gov>
2025-04-15 09:31:29 -0400

    switched example driver to slsqp

--- aviary/examples/run_off_design_example.py
+++ aviary/examples/run_off_design_example.py
@@ -117,10 +117,7 @@ prob.add_post_mission_systems()
 
 # Link phases and variables
 prob.link_phases()
-if env_truthy("TESTFLO_RUNNING"):
-    prob.add_driver('IPOPT', max_iter=50)
-else:
-    prob.add_driver('SLSQP', max_iter=50)
+prob.add_driver('SLSQP', max_iter=50)
 prob.add_design_variables()
 
 # Load optimization problem formulation
