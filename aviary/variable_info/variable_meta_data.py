304a828e7815b835684646b1e71a4e1c6c3dfa3a
Xun Jiang <xun.j.jiang@nasa.gov>
2025-04-23 18:23:29 -0400

    manual merge variable_meta_data.py

--- aviary/variable_info/variable_meta_data.py
+++ aviary/variable_info/variable_meta_data.py
@@ -7096,11 +7096,7 @@ add_meta_data(
 add_meta_data(
     Mission.Design.FUEL_MASS_REQUIRED,
     meta_data=_MetaData,
-<<<<<<< HEAD
     historical_name={"GASP": "INGASP.WFA", "FLOPS": None, "LEAPS1": None},
-=======
-    historical_name={'GASP': 'INGASP.WFAREQ', 'FLOPS': None, 'LEAPS1': None},
->>>>>>> 1e29f27c9fbe1b9370d442466cf8ab16f82b4583
     units='lbm',
     desc='fuel carried by the aircraft when it is on the ramp at the '
     'beginning of the design mission',
