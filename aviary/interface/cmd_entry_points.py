790647c741b0dd967a0ee7019fa59ed8ec4c10ba
jkirk5 <jason.kirk@nasa.gov>
2025-05-23 16:54:47 -0400

    more interface adjustments for check

--- aviary/interface/cmd_entry_points.py
+++ aviary/interface/cmd_entry_points.py
@@ -7,7 +7,7 @@ from aviary.interface.download_models import _exec_hangar, _setup_hangar_parser
 from aviary.interface.graphical_input import _exec_flight_profile, _setup_flight_profile_parser
 from aviary.interface.methods_for_level1 import _exec_level1, _setup_level1_parser
 from aviary.interface.plot_drag_polar import _exec_plot_drag_polar, _setup_plot_drag_polar_parser
-from aviary.interface.test_installation import _exec_installation_test  # , _setup_installation_test
+from aviary.interface.test_installation import _exec_installation_test, _setup_installation_test
 from aviary.utils.aero_table_conversion import _exec_ATC, _setup_ATC_parser
 from aviary.utils.engine_deck_conversion import EDC_description, _exec_EDC, _setup_EDC_parser
 from aviary.utils.fortran_to_aviary import _exec_F2A, _setup_F2A_parser
@@ -45,7 +45,7 @@ def _load_and_exec(script_name, user_args):
 
 _command_map = {
     'check': (
-        # _setup_installation_test,
+        _setup_installation_test,
         _exec_installation_test,
         'Verifies Aviary installation',
     ),
