26527a9a31fcc3d33b5a74a4101cb6e6bb01e6fa
jkirk5 <jason.kirk@nasa.gov>
2025-05-27 12:21:16 -0400

    pass correct arguments to installation test script unittest

--- aviary/interface/test/test_installation_test.py
+++ aviary/interface/test/test_installation_test.py
@@ -5,7 +5,7 @@ from aviary.interface.test_installation import _exec_installation_test
 
 class test_installation(unittest.TestCase):
     def test_installation(self):
-        success = _exec_installation_test()
+        success = _exec_installation_test(None, None)
         assert success
 
 
