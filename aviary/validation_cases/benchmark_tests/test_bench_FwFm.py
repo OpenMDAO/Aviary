6a96fef1c92d85c7f72c9b2573a024fa6013e0ab
jkirk5 <jason.kirk@nasa.gov>
2025-05-28 11:33:47 -0400

    removed accidental double nesting of tempdir decorator

--- aviary/validation_cases/benchmark_tests/test_bench_FwFm.py
+++ aviary/validation_cases/benchmark_tests/test_bench_FwFm.py
@@ -201,7 +201,6 @@ class ProblemPhaseTestCase(unittest.TestCase):
         _clear_problem_names()  # need to reset these to simulate separate runs
 
 
-@use_tempdirs
 class TestBenchFwFmSerial(ProblemPhaseTestCase):
     """Run the model in serial that is setup in ProblemPhaseTestCase class."""
 
@@ -236,7 +235,6 @@ class TestBenchFwFmSerial(ProblemPhaseTestCase):
         self.assertGreater(overall_fuel, 40000.0)
 
 
-@use_tempdirs
 @unittest.skipUnless(MPI and PETScVector, 'MPI and PETSc are required.')
 class TestBenchFwFmParallel(ProblemPhaseTestCase):
     """Run the model in parallel that is setup in ProblemPhaseTestCase class."""
