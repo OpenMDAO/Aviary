a7a2075ad45d44be3572add679c05347f2aac928
Monstertov <rob@monstertov.nl>
2025-05-29 19:53:48 +0200

    Improved clarity by fixing typo: 'unneccesary' corrected to 'unnecessary'

--- aviary/core/AviaryGroup.py
+++ aviary/core/AviaryGroup.py
@@ -104,7 +104,7 @@ class AviaryGroup(om.Group):
             # in the state input component. Since this solver can cause some
             # numerical problems, and can slow things down, we need to move it down
             # into the state interp component.
-            # TODO: Future updates to dymos may make this unneccesary.
+            # TODO: Future updates to dymos may make this unnecessary.
             for phase in self.traj.phases.system_iter(recurse=False):
                 # Don't move the solvers if we are using solve segements.
                 if phase_info[phase.name]['user_options'].get('solve_for_distance'):
