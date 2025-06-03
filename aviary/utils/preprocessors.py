318e6bdfa962c1afa757d0024b78b8ec0790de14
Xun Jiang <xun.j.jiang@nasa.gov>
2025-06-03 19:38:07 -0400

    run preprocess_propulsion only when engine_models is not None

--- aviary/utils/preprocessors.py
+++ aviary/utils/preprocessors.py
@@ -43,7 +43,8 @@ def preprocess_options(aviary_options: AviaryValues, meta_data=_MetaData, verbos
             aviary_options.set_val(Settings.VERBOSITY, verbosity)
 
     preprocess_crewpayload(aviary_options, meta_data, verbosity)
-    preprocess_propulsion(aviary_options, engine_models, meta_data, verbosity)
+    if not engine_models is None:
+        preprocess_propulsion(aviary_options, engine_models, meta_data, verbosity)
 
 
 def remove_preprocessed_options(aviary_options):
