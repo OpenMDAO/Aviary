639ee118997b248a4ded259dfe1d7d0fe9839b97
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2025-06-10 10:44:33 -0400

    Fix for backwards incompatible dymos change on control rate units

--- aviary/mission/flight_phase_builder.py
+++ aviary/mission/flight_phase_builder.py
@@ -408,7 +408,7 @@ class FlightPhaseBase(PhaseBuilderBase):
         control_dict = {
             'name': Dynamic.Atmosphere.MACH,
             'targets': Dynamic.Atmosphere.MACH,
-            # 'units': mach_bounds[1],
+            #'units': 'unitless',
             'rate_targets': rate_targets,
             'opt': optimize_mach,
         }
