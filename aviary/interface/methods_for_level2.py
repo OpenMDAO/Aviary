bcc7b984926e0c1ba3aae9c0bdba1a57b5abf923
Alemu <aalemu1@ndc.nasa.gov>
2025-06-11 11:28:34 -0400

    mission_range correction in off_design

--- aviary/interface/methods_for_level2.py
+++ aviary/interface/methods_for_level2.py
@@ -2009,15 +2009,20 @@ class AviaryProblem(om.Problem):
             num_first = num_business = num_tourist = wing_cargo = misc_cargo = 0
 
         if phase_info is None:
+            #Somewhere between the sizing and off-design self.pre_mission_info gets deleted
             phase_info = self.phase_info
+            phase_info['pre_mission']=self.pre_mission_info
+            phase_info['post_mission']=self.post_mission_info
         if mission_range is None:
             # mission range is sliced from a column vector numpy array, i.e. it is a len
             # 1 numpy array
-            mission_range = self.get_val(Mission.Design.RANGE)[0]
+            #fixes issue wherein an alternate mission with no inputs "alternate_mission()" attempts to run the range
+            #within the CSV file instead of in the phase_info.
+            mission_range = self.phase_info['post_mission']['target_range'][0]
 
         # gross mass is sliced from a column vector numpy array, i.e. it is a len 1 numpy
         # array
-        mission_mass = self.get_val(Mission.Design.GROSS_MASS)
+        mission_mass = self.get_val(Mission.Design.GROSS_MASS)[0]
         optimizer = self.driver.options['optimizer']
 
         prob_alternate = _load_off_design(
@@ -2515,15 +2520,10 @@ def _load_off_design(
                     'no specified Range'
                 )
         else:
-            prob.aviary_inputs.set_val(Mission.Design.RANGE, mission_range, units='NM')
+            
+            """prob.aviary_inputs.set_val(Mission.Design.RANGE, mission_range, units='NM')"""
             prob.aviary_inputs.set_val(Mission.Summary.RANGE, mission_range, units='NM')
-            # TODO is there a reason we can't use set_default() to make sure target range exists and
-            #      has a value if not already in dictionary?
-            try:
-                phase_info['post_mission']['target_range']
-                phase_info['post_mission']['target_range'] = (mission_range, 'nmi')
-            except KeyError:
-                warnings.warn('no target range to update')
+            phase_info['post_mission']['target_range'] = (mission_range, 'nmi')
 
     elif problem_type == ProblemType.FALLOUT:
         # Set mission fuel and calculate gross weight, aviary will calculate range
