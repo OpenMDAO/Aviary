123aa3403458e89f61c1964365a5895efe4b1646
Xun Jiang <xun.j.jiang@nasa.gov>
2025-06-20 11:23:54 -0400

    updated a couple outputs in MassSummationTestCase4

--- aviary/subsystems/mass/gasp_based/test/test_mass_summation.py
+++ aviary/subsystems/mass/gasp_based/test/test_mass_summation.py
@@ -1168,9 +1168,9 @@ class MassSummationTestCase4(unittest.TestCase):
         )  # modified from GASP value to account for updated crew mass. GASP value is 78966.7
 
         assert_near_equal(
-            self.prob['fuel_mass.fus_mass_full'], 102686.89, tol
+            self.prob['fuel_mass.fus_mass_full'], 101867.46, tol
         )  # calculated by hand,  #modified from GASP value to account for updated crew mass. GASP value is 102501.95695930265
-        assert_near_equal(self.prob[Aircraft.Fuel.FUEL_SYSTEM_MASS], 1923.73, tol)
+        assert_near_equal(self.prob[Aircraft.Fuel.FUEL_SYSTEM_MASS], 1960.68, tol)
         assert_near_equal(
             self.prob['fuel_mass.fuel_and_oem.OEM_wingfuel_mass'], 79474.11569854, tol
         )  # modified from GASP value to account for updated crew mass. GASP value is 78966.7
