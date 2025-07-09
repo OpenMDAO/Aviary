f85f17818bb427693463670ad66c393c35da24bb
szoppelt <145373267+szoppelt@users.noreply.github.com>
2025-07-09 10:34:37 -0400

    Update test_wing.py

--- aviary/subsystems/mass/simple_mass/test/test_wing.py
+++ aviary/subsystems/mass/simple_mass/test/test_wing.py
@@ -75,7 +75,7 @@ class WingMassTestCase(unittest.TestCase):
 
         tol = 1e-10
         assert_near_equal(self.prob[Aircraft.Wing.MASS], 
-                          4.22032, 
+                          10.6966719, 
                           tol) 
         
         partial_data = self.prob.check_partials(
