bdfb6e3f822197198d7a875fd14f6f63096e46b3
Xun Jiang <xun.j.jiang@nasa.gov>
2025-07-11 11:29:26 -0400

    alter promotes

--- aviary/subsystems/mass/gasp_based/mass_premission.py
+++ aviary/subsystems/mass/gasp_based/mass_premission.py
@@ -19,6 +19,39 @@ class MassPremission(om.Group):
     def setup(self):
         design_type = self.options[Aircraft.Design.TYPE]
 
+        # output values from design_load that are connected to fixed_mass via promotion
+        fixed_mass_design_load_values = ['max_mach', 'min_dive_vel']
+
+        # output values from fixed_mass that are connected to wing_mass via promotion
+        wing_mass_fixed_mass_values = [
+            'c_strut_braced',
+            'c_gear_loc',
+            'half_sweep',
+        ]
+
+        # output values from design_load that are connected to fuel_mass via promotion
+        fuel_mass_design_load_values = ['min_dive_vel']
+
+        # output values from fixed_mass that are connected to fuel_mass via promotion
+        fuel_mass_fixed_mass_values = [
+            'payload_mass_des',
+            'payload_mass_max',
+            'wing_mounted_mass',
+            'eng_comb_mass',
+        ]
+
+        # combine all necessary inputs and outputs for each group
+
+        fixed_mass_inputs = fixed_mass_design_load_values + ['density']
+        fixed_mass_outputs = wing_mass_fixed_mass_values + fuel_mass_fixed_mass_values
+
+        wing_mass_inputs = wing_mass_fixed_mass_values
+
+        if design_type is AircraftTypes.BLENDED_WING_BODY:
+            fuel_mass_inputs = fuel_mass_fixed_mass_values
+        else:
+            fuel_mass_inputs = fuel_mass_design_load_values + fuel_mass_fixed_mass_values
+
         # create the instances of the groups
 
         if design_type is AircraftTypes.BLENDED_WING_BODY:
@@ -39,8 +72,8 @@ class MassPremission(om.Group):
         self.add_subsystem(
             'fixed_mass',
             FixedMassGroup(),
-            promotes_inputs=['*'],
-            promotes_outputs=['*'],
+            promotes_inputs=fixed_mass_inputs + ['*'],
+            promotes_outputs=fixed_mass_outputs + ['*'],
         )
 
         self.add_subsystem(
@@ -54,20 +87,20 @@ class MassPremission(om.Group):
             self.add_subsystem(
                 'wing_mass',
                 BWBWingMassGroup(),
-                promotes_inputs=['*'],
+                promotes_inputs=wing_mass_inputs + ['*'],
                 promotes_outputs=['*'],
             )
         else:
             self.add_subsystem(
                 'wing_mass',
                 WingMassGroup(),
-                promotes_inputs=['*'],
+                promotes_inputs=wing_mass_inputs + ['*'],
                 promotes_outputs=['*'],
             )
 
         self.add_subsystem(
             'fuel_mass',
             FuelMassGroup(),
-            promotes_inputs=['*'],
+            promotes_inputs=fuel_mass_inputs + ['*'],
             promotes_outputs=['*'],
         )
