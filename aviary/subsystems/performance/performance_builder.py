03e993c97da4efebc42dfea77a3f35086723c25b
jkirk5 <jason.kirk@nasa.gov>
2025-07-24 14:01:32 -0400

    just performance premission

--- aviary/subsystems/performance/performance_builder.py
+++ aviary/subsystems/performance/performance_builder.py
@@ -1,21 +1,5 @@
-import openmdao.api as om
-
-from aviary.subsystems.performance.ode.load_factor import LoadFactor
-from aviary.subsystems.subsystem_builder_base import SubsystemBuilderBase
 from aviary.subsystems.performance.performance_premission import PerformancePremission
-from aviary.variable_info.variables import Aircraft, Mission, Dynamic
-
-analysis_dict = {'sustained_load_factor': LoadFactor}
-conditions_dict = {
-    'altitude': Dynamic.Mission.ALTITUDE,
-    'velocity': Dynamic.Mission.VELOCITY,
-    'mach': Dynamic.Atmosphere.MACH,
-    'throttle': Dynamic.Vehicle.Propulsion.THROTTLE,
-    'mass': Dynamic.Vehicle.MASS,
-    'fuel_fraction': 'fuel_fraction',
-    'delta_weight': 'delta_weight',
-    'load_factor': 'load_factor',
-}
+from aviary.subsystems.subsystem_builder_base import SubsystemBuilderBase
 
 
 class CorePerformanceBuilder(SubsystemBuilderBase):
@@ -29,19 +13,3 @@ class CorePerformanceBuilder(SubsystemBuilderBase):
 
     def build_pre_mission(self, aviary_inputs, **kwargs):
         return PerformancePremission()
-
-    def build_post_mission(self, num_nodes, aviary_inputs, **kwargs):
-        if len(kwargs) > 0:
-            analyses = [analyses]
-            perf_group = om.Group()
-        else:
-            analyses = []
-            perf_group = None
-
-        for perf_analysis in kwargs:
-            conditions = kwargs[perf_analysis]
-            perf_comp = analysis_dict[perf_analysis]
-            # set_input_defaults on perf_comp based on conditions
-            perf_group.add_subsystem(name=perf_analysis, subsys=perf_comp(), promotes=['*'])
-
-        return perf_group
