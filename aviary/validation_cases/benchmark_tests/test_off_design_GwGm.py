8986c2986853b6f20fdbe89a17abe7ff9e21e104
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2025-08-08 18:35:53 -0400

    Update design gross mass in off-design case to account for lower drag from Xun's bug fix in form_factor.

--- aviary/validation_cases/benchmark_tests/test_off_design_GwGm.py
+++ aviary/validation_cases/benchmark_tests/test_off_design_GwGm.py
@@ -11,7 +11,7 @@ from aviary.variable_info.enums import ProblemType, Verbosity
 
 class TwoDOFTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.sized_mass = 174039.0
+        self.sized_mass = 171044.0
         self.sized_range = 3675
         self.phase_info = deepcopy(phase_info)
 
