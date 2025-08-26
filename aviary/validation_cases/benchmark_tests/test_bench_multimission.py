75a0fb2c043ff22f6bf27f1e4431a365194b9802
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2025-08-26 16:53:01 -0400

    Some new values from using polynomial order 3 instead of 1 in climb/descent for multimission.

--- aviary/validation_cases/benchmark_tests/test_bench_multimission.py
+++ aviary/validation_cases/benchmark_tests/test_bench_multimission.py
@@ -20,13 +20,13 @@ class MultiMissionTestcase(unittest.TestCase):
         prob = multi_mission_example()
 
         objective = prob.get_val('composite_objective', units=None)
-        objective_expected_value = 25672.61271768
+        objective_expected_value = 24851.7
 
         mission1_fuel = prob.get_val('mission1.mission:summary:fuel_burned', units='lbm')
-        mission1_fuel_expected_value = 27050.67106054
+        mission1_fuel_expected_value = 26211.5
 
         mission2_fuel = prob.get_val('mission2.mission:summary:fuel_burned', units='lbm')
-        mission2_fuel_expected_value = 22916.49603196
+        mission2_fuel_expected_value = 22132.2
 
         mission1_cargo = prob.get_val(
             'mission1.aircraft:crew_and_payload:total_payload_mass', units='lbm'
