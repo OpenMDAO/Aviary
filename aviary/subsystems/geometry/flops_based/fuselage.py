89b108f12ab2a38e5703abe447a9e0d17a929112
Xun Jiang <xun.j.jiang@nasa.gov>
2025-09-04 11:04:42 -0400

    work in progress

--- aviary/subsystems/geometry/flops_based/fuselage.py
+++ aviary/subsystems/geometry/flops_based/fuselage.py
@@ -117,23 +117,31 @@ class DetailedCabinLayout(om.ExplicitComponent):
 
     def initialize(self):
         add_aviary_option(self, Settings.VERBOSITY)
+        add_aviary_option(self, Aircraft.Fuselage.NUM_FUSELAGES)
 
     def setup(self):
-        add_aviary_input(self, Aircraft.Fuselage.LENGTH, units='ft')
+        add_aviary_output(self, Aircraft.Fuselage.LENGTH, units='ft')
         add_aviary_output(self, Aircraft.Fuselage.PASSENGER_COMPARTMENT_LENGTH, units='ft')
+        add_aviary_output(self, Aircraft.Fuselage.MAX_WIDTH, units='ft')
+        add_aviary_output(self, Aircraft.Fuselage.MAX_HEIGHT, units='ft')
 
     def compute(self, inputs, outputs):
         verbosity = self.options[Settings.VERBOSITY]
         num_first_class_pax = self.options[Aircraft.CrewPayload.Design.NUM_FIRST_CLASS]
         num_tourist_class_pax = self.options[Aircraft.CrewPayload.Design.NUM_TOURIST_CLASS]
         fuselage_multiplier = 1.0
+        num_fuselage = self.options[Aircraft.Fuselage.NUM_FUSELAGES]
+        if num_fuselage > 1:
+            if verbosity > Verbosity.BRIEF:
+                print('Multiple fuselage configuration is not implemented yet.')
 
+        # The 200 was derived from 757, the largest single aisle western desig.
         if num_tourist_class_pax > 200:
             num_seat_abreast_tourist = self.options[Aircraft.Fuselage.NUM_SEATS_ABREAST_TOURIST]
             if num_seat_abreast_tourist <= 0:
                 num_seat_abreast_tourist = 8
             num_seat_abreast_first = self.options[Aircraft.Fuselage.NUM_SEATS_ABREAST_FIRST]
-            if num_seat_abreast_first <= 0:
+            if num_seat_abreast_first > 0 and num_seat_abreast_first <= 0:
                 num_seat_abreast_first = num_seat_abreast_tourist - 2
 
         if num_seat_abreast_first <= 0 and num_first_class_pax > 0:
@@ -141,54 +149,115 @@ class DetailedCabinLayout(om.ExplicitComponent):
         if num_seat_abreast_tourist <= 0 and num_tourist_class_pax > 0:
             num_seat_abreast_tourist = 6
 
+        # Though these are not user definable, the values here are typical for most transport
         aisle_width_first_class = 20.0  # inch
         aisle_width_tourist_class = 18.0  # inch
 
+        # If there are less than 60 passengers on board, then the aisle should be slightly narrow.
+        # Also, if the number of passengers abreast was not specified, then set it to 5 as 6 is too much
+        # for a typical short range transport.
         if num_tourist_class_pax < 60:
             if num_seat_abreast_tourist <= 0:
                 num_seat_abreast_tourist = 5
             aisle_width_tourist_class = 15.0
 
-        num_aisles = 1
         if num_seat_abreast_tourist > 6:
             num_aisles = 2
+        else:
+            num_aisles = 1
 
+        # Even though the widebody gives you more room, the aisles in it are a little narrower.
+        # That is what is set here. It is assumed that if the number of abreast is greater than
+        # 4 or 6 as shown below that we are working with a widebody aircraft.
         if num_seat_abreast_first > 4:
             aisle_width_first_class = 18.0
         if num_seat_abreast_tourist > 6:
             aisle_width_tourist_class = 15.0
 
         seat_pitch_first = self.options[Aircraft.Fuselage.SEAT_PITCH_FIRST]
-        seat_pitch_tourist = self.options[Aircraft.Fuselage.SEAT_PITCH_TOURIST]
-
         if seat_pitch_first <= 0 and num_first_class_pax > 0:
             seat_pitch_first = 38.0  # inch
+        seat_pitch_tourist = self.options[Aircraft.Fuselage.SEAT_PITCH_TOURIST]
         if seat_pitch_tourist <= 0 and num_tourist_class_pax > 0:
             seat_pitch_tourist = 34.0  # inch
 
+        # Set constraints on the maximum number of galleys and other items so that we don't have a
+        # flying kitchen or closet or whatever.
+        # Note: Some of these may need relaxing for larger aircraft.
         if num_first_class_pax == 0:
-            max_lav = 3
             design_range = inputs[Mission.Design.RANGE]
             if design_range < 1250:
                 max_lav = 2
+            else:
+                max_lav = 3
             if num_tourist_class_pax < 180:
                 fuselage_multiplier = 0.910
-            max_Galleys = 2
+            max_galleys = 2
             max_closets = 2
 
         if num_first_class_pax == 0 and num_tourist_class_pax < 110:
             max_lav = 1
-            max_Galleys = 1
+            max_galleys = 1
             max_closets = 1
 
         if num_tourist_class_pax > 320:
             max_lav = 4
-            max_Galleys = 4
+            max_galleys = 4
             max_closets = 4
         elif num_tourist_class_pax > 600:
             max_lav = 8
-            max_Galleys = 8
+            max_galleys = 8
             max_closets = 8
 
         if num_first_class_pax > 0 and num_seat_abreast_tourist < 8:
             fuselage_multiplier = 0.950
+
+        # Calculate the number of galleys, lavatories and closets
+        num_galleys = int(1 + ((num_first_class_pax + num_tourist_class_pax) / 100))
+        if num_galleys > max_galleys:
+            num_galleys = max_galleys
+        num_lavas = int(1 + (num_tourist_class_pax / 60)) + int(1 + (num_first_class_pax / 100))
+        if num_lavas > max_lav:
+            num_lavas = max_lav
+        num_closets = int(1 + (num_tourist_class_pax / 30)) + int(1 + (num_first_class_pax / 60))
+        if num_closets > max_closets:
+            num_closets = max_closets
+
+        # Calculate the passenger compartment length
+
+        num_engines = self.options[Aircraft.Engine.NUM_ENGINES]
+        eng_flag = num_engines - 2 * int(num_engines / 2)  # a center mounted engine if 1.
+        first_class_len = num_first_class_pax * seat_pitch_first / num_seat_abreast_first
+        tourist_class_len = num_tourist_class_pax * seat_pitch_tourist / num_seat_abreast_tourist
+        pax_compart_length = (
+            first_class_len
+            + (num_galleys + num_lavas) * 36.0
+            + tourist_class_len
+            + num_closets * 12.0
+        ) / 12.0
+
+        # Correct for doors that may be in the way
+        num_doors = 1 + int((pax_compart_length / 50.0) * num_seat_abreast_tourist / 6.0)
+
+        # Final passenger compartment length
+        pax_compart_length = (pax_compart_length + num_doors * 2.96) * fuselage_multiplier
+        fuselage_length = pax_compart_length + 25.0 * eng_flag + 40.0
+        outputs[Aircraft.Fuselage.PASSENGER_COMPARTMENT_LENGTH] = pax_compart_length
+        outputs[Aircraft.Fuselage.LENGTH] = fuselage_length
+
+        # Calculate the number of rows of each class of passenger (not needed)
+        if num_first_class_pax > 0:
+            num_rows_first = int(num_first_class_pax / num_seat_abreast_first)
+        if num_tourist_class_pax > 0:
+            num_rows_tourist = int(num_tourist_class_pax / num_seat_abreast_tourist)
+
+        # Calculate the fuselage width of the passenger seats
+        width_first_class = (
+            num_aisles * aisle_width_first_class + num_seat_abreast_first * 20.0
+        ) / 12.0
+        width_tourist_class = (
+            num_aisles * aisle_width_tourist_class + num_seat_abreast_tourist * 25.0
+        ) / 12.0
+        width_fuselage = np.max(width_first_class, width_tourist_class) * 1.06
+        outputs[Aircraft.Fuselage.MAX_WIDTH] = width_fuselage
+        outputs[Aircraft.Fuselage.MAX_HEIGHT] = width_fuselage + 0.9
