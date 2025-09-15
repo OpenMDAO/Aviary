87e6fb8c29af552f05575d4d31c445a372af733b
cmbenne3 <christopher.m.bennett@nasa.gov>
2025-09-15 17:18:40 -0400

    updating test_bwb_GwGm.py to use default 2dof phase info to ensure the problem converges

--- aviary/validation_cases/benchmark_tests/test_bwb_GwGm.py
+++ aviary/validation_cases/benchmark_tests/test_bwb_GwGm.py
@@ -5,9 +5,8 @@ from openmdao.core.problem import _clear_problem_names
 from openmdao.utils.assert_utils import assert_near_equal
 from openmdao.utils.testing_utils import require_pyoptsparse, use_tempdirs
 
-from aviary.models.aircraft.blended_wing_body.generic_BWB_phase_info import (
-    two_dof_phase_info,
-)
+from aviary.models.missions.two_dof_default import phase_info
+
 from aviary.interface.methods_for_level1 import run_aviary
 from aviary.variable_info.variables import Aircraft, Mission
 
@@ -25,7 +24,7 @@ class ProblemPhaseTestCase(unittest.TestCase):
 
     @require_pyoptsparse(optimizer='SNOPT')
     def test_bench_GwGm(self):
-        local_phase_info = deepcopy(two_dof_phase_info)
+        local_phase_info = deepcopy(phase_info)
         prob = run_aviary(
             'models/aircraft/blended_wing_body/generic_BWB_GASP.csv',
             local_phase_info,
@@ -40,25 +39,25 @@ class ProblemPhaseTestCase(unittest.TestCase):
         # There are no truth values for these.
         assert_near_equal(
             prob.get_val(Mission.Design.GROSS_MASS, units='lbm'),
-            151764.0407,
+            141260.50569975,
             tolerance=rtol,
         )  # WG = 150000.0
 
         assert_near_equal(
             prob.get_val(Aircraft.Design.OPERATING_MASS, units='lbm'),
-            82444.5349,
+            80181.28382411,
             tolerance=rtol,
         )  # OWE = 82982.0
 
         assert_near_equal(
             prob.get_val(Mission.Summary.TOTAL_FUEL_MASS, units='lbm'),
-            35644.345,
+            27329.22187564,
             tolerance=rtol,
         )  # WFA = 33268.0
 
         assert_near_equal(
             prob.get_val(Mission.Landing.GROUND_DISTANCE, units='ft'),
-            2129.3846,
+            2204.60962657,
             tolerance=rtol,
         )  # DLT
 
@@ -66,7 +65,7 @@ class ProblemPhaseTestCase(unittest.TestCase):
 
         assert_near_equal(
             prob.get_val(Mission.Landing.TOUCHDOWN_MASS, units='lbm'),
-            119428.3283,
+            116415.96054007,
             tolerance=rtol,
         )
 
