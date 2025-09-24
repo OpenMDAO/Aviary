8a7d6f5199762c3d76486549f7e9ad9b381c7e5d
jkirk5 <jason.kirk@nasa.gov>
2025-09-24 13:39:41 -0400

    PR feedback

--- aviary/examples/run_level3_example.py
+++ aviary/examples/run_level3_example.py
@@ -496,37 +496,6 @@ prob.set_val(Mission.Summary.GROSS_MASS, 175400, units='lbm')
 prob.verbosity = Verbosity.BRIEF
 
 prob.run_aviary_problem()
+
 # prob.model.list_vars(units=True, print_arrays=True)
-# prob.list_driver_vars(
-#     print_arrays=True,
-#     desvar_opts=[
-#         'lower',
-#         'upper',
-#         'ref',
-#         'ref0',
-#         'indices',
-#         'adder',
-#         'scaler',
-#         'parallel_deriv_color',
-#         'cache_linear_solution',
-#         'units',
-#         'min',
-#         'max',
-#     ],
-#     cons_opts=[
-#         'lower',
-#         'upper',
-#         'equals',
-#         'ref',
-#         'ref0',
-#         'indices',
-#         'adder',
-#         'scaler',
-#         'linear',
-#         'parallel_deriv_color',
-#         'cache_linear_solution',
-#         'units',
-#         'min',
-#         'max',
-#     ],
-# )
+# prob.list_driver_vars(print_arrays=True)
