f2daa12967e46f556f2d768c9d18cc0e0fd3b691
Xun Jiang <xun.j.jiang@nasa.gov>
2025-10-07 13:35:06 -0400

    add BWBTransportAirCondMassTest

--- aviary/subsystems/mass/flops_based/test/test_air_conditioning.py
+++ aviary/subsystems/mass/flops_based/test/test_air_conditioning.py
@@ -2,6 +2,7 @@ import unittest
 
 import openmdao.api as om
 from openmdao.utils.assert_utils import assert_check_partials
+from openmdao.utils.testing_utils import use_tempdirs
 from parameterized import parameterized
 
 from aviary.subsystems.mass.flops_based.air_conditioning import AltAirCondMass, TransportAirCondMass
@@ -16,6 +17,7 @@ from aviary.validation_cases.validation_tests import (
 from aviary.variable_info.variables import Aircraft
 
 
+@use_tempdirs
 class TransportAirCondMassTest(unittest.TestCase):
     def setUp(self):
         self.prob = om.Problem()
@@ -93,6 +95,7 @@ class TransportAirCondMassTest2(unittest.TestCase):
         assert_check_partials(partial_data, atol=1e-12, rtol=1e-12)
 
 
+@use_tempdirs
 class AltAirCondMassTest(unittest.TestCase):
     """Tests alternate air conditioning mass calculation."""
 
@@ -159,6 +162,43 @@ class AltAirCondMassTest2(unittest.TestCase):
 
         partial_data = prob.check_partials(out_stream=None, method='cs')
         assert_check_partials(partial_data, atol=1e-12, rtol=1e-12)
+
+
+@use_tempdirs
+class BWBTransportAirCondMassTest(unittest.TestCase):
+    def setUp(self):
+        self.prob = om.Problem()
+
+    def test_case(self):
+        case_name = 'BWB1aFLOPS'
+        prob = self.prob
+
+        prob.model.add_subsystem(
+            'air_cond',
+            TransportAirCondMass(),
+            promotes_inputs=['*'],
+            promotes_outputs=['*'],
+        )
+
+        prob.model_options['*'] = get_flops_options(case_name)
+
+        prob.setup(check=False, force_alloc_complex=True)
+
+        flops_validation_test(
+            prob,
+            case_name,
+            input_keys=[
+                Aircraft.AirConditioning.MASS_SCALER,
+                Aircraft.Avionics.MASS,
+                Aircraft.Fuselage.MAX_HEIGHT,
+                Aircraft.Fuselage.PLANFORM_AREA,
+            ],
+            output_keys=Aircraft.AirConditioning.MASS,
+            aviary_option_keys=[Aircraft.CrewPayload.Design.NUM_PASSENGERS],
+            version=Version.TRANSPORT,
+            tol=3.0e-4,
+            atol=1e-11,
+        )
 
 
 if __name__ == '__main__':
