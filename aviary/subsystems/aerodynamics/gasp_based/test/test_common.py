a4ba746a85266cde075c35f3fea5e177dca1a6cd
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2025-10-08 15:11:13 -0400

    ZZRevert "isolate"
    This reverts commit 6d603ed52d69a108bd0728a1dbdda6858e44f5cd.

--- aviary/subsystems/aerodynamics/gasp_based/test/test_common.py
+++ aviary/subsystems/aerodynamics/gasp_based/test/test_common.py
@@ -20,7 +20,7 @@ class TestTanhRampComp(unittest.TestCase):
     def test_tanh_ramp_up(self):
         p = om.Problem()
 
-        nn = 100
+        nn = 99
 
         c = TanhRampComp(time_units='s', num_nodes=nn)
 
