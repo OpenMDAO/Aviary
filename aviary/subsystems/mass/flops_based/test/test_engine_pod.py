395a319de02ccd3cced69cd00326be09e774da48
Xun Jiang <xun.j.jiang@nasa.gov>
2025-10-09 12:58:44 -0400

    work in progress on engine_pod

--- aviary/subsystems/mass/flops_based/test/test_engine_pod.py
+++ aviary/subsystems/mass/flops_based/test/test_engine_pod.py
@@ -3,11 +3,13 @@ import unittest
 import numpy as np
 import openmdao.api as om
 from openmdao.utils.assert_utils import assert_check_partials, assert_near_equal
+from openmdao.utils.testing_utils import use_tempdirs
 from parameterized import parameterized
 
 from aviary.subsystems.mass.flops_based.engine_pod import EnginePodMass
 from aviary.utils.test_utils.variable_test import assert_match_varnames
 from aviary.validation_cases.validation_tests import (
+    Version,
     flops_validation_test,
     get_flops_case_names,
     get_flops_inputs,
@@ -16,6 +18,7 @@ from aviary.validation_cases.validation_tests import (
 from aviary.variable_info.variables import Aircraft
 
 
+@use_tempdirs
 class EnginePodMassTest(unittest.TestCase):
     """Tests the engine pod mass needed for the detailed wing calculation."""
 
@@ -24,7 +27,9 @@ class EnginePodMassTest(unittest.TestCase):
 
     # Only cases that use detailed wing weight.
     @parameterized.expand(
-        get_flops_case_names(omit=['LargeSingleAisle2FLOPS', 'LargeSingleAisle2FLOPSalt']),
+        get_flops_case_names(
+            omit=['LargeSingleAisle2FLOPS', 'LargeSingleAisle2FLOPSalt', 'BWB1aFLOPS']
+        ),
         name_func=print_case,
     )
     def test_case(self, case_name):
@@ -100,8 +105,56 @@ class EnginePodMassTest(unittest.TestCase):
         assert_match_varnames(self.prob.model)
 
 
+class BWBEnginePodMassTest(unittest.TestCase):
+    """Tests the engine pod mass needed for the detailed wing calculation."""
+
+    def setUp(self):
+        self.prob = om.Problem()
+
+    def test_case(self):
+        case_name = 'BWB1aFLOPS'
+        prob = self.prob
+
+        inputs = get_flops_inputs(case_name, preprocess=True)
+
+        options = {
+            Aircraft.Engine.NUM_ENGINES: inputs.get_val(Aircraft.Engine.NUM_ENGINES),
+        }
+
+        prob.model.add_subsystem(
+            'engine_pod', EnginePodMass(), promotes_outputs=['*'], promotes_inputs=['*']
+        )
+
+        prob.model_options['*'] = options
+
+        prob.setup(check=False, force_alloc_complex=True)
+
+        # Tol not that tight, but it is unclear where the pod mass values in files come from,
+        # since they aren't printed in the FLOPS output.
+        flops_validation_test(
+            prob,
+            case_name,
+            input_keys=[
+                Aircraft.Electrical.MASS,
+                Aircraft.Fuel.FUEL_SYSTEM_MASS,
+                Aircraft.Hydraulics.MASS,
+                Aircraft.Instruments.MASS,
+                Aircraft.Nacelle.MASS,
+                Aircraft.Propulsion.TOTAL_ENGINE_CONTROLS_MASS,
+                Aircraft.Engine.MASS,
+                Aircraft.Propulsion.TOTAL_STARTER_MASS,
+                Aircraft.Engine.THRUST_REVERSERS_MASS,
+                Aircraft.Engine.SCALED_SLS_THRUST,
+                Aircraft.Propulsion.TOTAL_SCALED_SLS_THRUST,
+            ],
+            output_keys=Aircraft.Engine.POD_MASS,
+            version=Version.BWB,  # TODO: Version.BWB,
+            tol=3e-3,
+        )
+
+
 if __name__ == '__main__':
-    unittest.main()
-    # test = EnginePodMassTest()
-    # test.setUp()
-    # test.test_case_multiengine()
+    # unittest.main()
+    test = BWBEnginePodMassTest()
+    test.setUp()
+    test.test_case()
