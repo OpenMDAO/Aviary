bf5df3a8f6bcb98ea8a0a20378492e519feba64a
jkirk5 <jason.kirk@nasa.gov>
2026-01-06 13:19:45 -0500

    report update

--- aviary/subsystems/mass/mass_builder.py
+++ aviary/subsystems/mass/mass_builder.py
@@ -91,53 +91,10 @@ class CoreMassBuilder(MassBuilder):
         filename = self.name + '.md'
         filepath = reports_folder / filename
 
-        # outputs = [
-        #     Aircraft.Wing.MASS,
-        #     Aircraft.Design.EMPENNAGE_MASS,
-        #     Aircraft.Canard.MASS,
-        #     Aircraft.HorizontalTail.MASS,
-        #     Aircraft.VerticalTail.MASS,
-        #     Aircraft.Fins.MASS,
-        #     Aircraft.Fuselage.MASS,
-        #     Aircraft.LandingGear.TOTAL_MASS,
-        #     Aircraft.LandingGear.MAIN_GEAR_MASS,
-        #     Aircraft.LandingGear.NOSE_GEAR_MASS,
-        #     Aircraft.Nacelle.MASS,
-        #     Aircraft.Design.STRUCTURE_MASS,
-        #     Aircraft.Propulsion.MASS,
-        #     Aircraft.Engine.MASS,
-        #     Aircraft.Engine.CONTROLS_MASS,
-        #     Aircraft.Engine.STARTER_MASS,
-        #     # Aircraft.Engine.Propeller.MASS
-        #     Aircraft.Fuel.FUEL_SYSTEM_MASS,
-        #     Aircraft.Design.SYSTEMS_AND_EQUIPMENT_MASS,
-        #     Aircraft.Controls.MASS,
-        #     Aircraft.APU.MASS,
-        #     Aircraft.Instruments.MASS,
-        #     Aircraft.Hydraulics.MASS,
-        #     Aircraft.Electrical.MASS,
-        #     Aircraft.Avionics.MASS,
-        #     Aircraft.Furnishings.MASS,
-        #     Aircraft.AirConditioning.MASS,
-        #     Aircraft.AntiIcing.MASS,
-        #     Aircraft.Design.EMPTY_MASS,
-        #     Mission.Summary.USEFUL_LOAD,
-        #     Aircraft.CrewPayload.FLIGHT_CREW_MASS,
-        #     Aircraft.CrewPayload.CABIN_CREW_MASS,
-        #     Aircraft.Fuel.UNUSABLE_FUEL_MASS,
-        #     Aircraft.Propulsion.TOTAL_ENGINE_OIL_MASS,
-        #     Mission.Summary.OPERATING_MASS,
-        #     Aircraft.CrewPayload.PASSENGER_MASS_TOTAL,
-        #     Aircraft.CrewPayload.CARGO_MASS,
-        #     Aircraft.CrewPayload.WING_CARGO,
-        #     Aircraft.CrewPayload.MISC_CARGO,
-        #     Mission.Summary.ZERO_FUEL_MASS,
-        #     Mission.Summary.FUEL_MASS,
-        #     Mission.Summary.GROSS_MASS,
-        # ]
-
         num_engines = prob.model.aviary_inputs.get_val(Aircraft.Engine.NUM_ENGINES)
         engine_models = prob.model.engine_builders
+
+        tab = '&nbsp;&nbsp;&nbsp;&nbsp;'
 
         with open(filepath, mode='w') as f:
             method = self.code_origin.value + '-derived relations'
@@ -156,21 +113,21 @@ class CoreMassBuilder(MassBuilder):
 
             val, units = find_variable_in_problem(Aircraft.Canard.MASS, prob, self.meta_data)
             if val != 0.0 or val != 'Not Found in Model':
-                f.write(f'|\tCanard|{val}|{units}|\n')
+                f.write(f'|{tab}Canard|{val}|{units}|\n')
 
             val, units = find_variable_in_problem(
                 Aircraft.HorizontalTail.MASS, prob, self.meta_data
             )
             if val != 0.0 or val != 'Not Found in Model':
-                f.write(f'|\tHorizontal Tail|{val}|{units}|\n')
+                f.write(f'|{tab}Horizontal Tail|{val}|{units}|\n')
 
             val, units = find_variable_in_problem(Aircraft.VerticalTail.MASS, prob, self.meta_data)
             if val != 0.0 or val != 'Not Found in Model':
-                f.write(f'|\tVertical Tail|{val}|{units}|\n')
+                f.write(f'|{tab}Vertical Tail|{val}|{units}|\n')
 
             val, units = find_variable_in_problem(Aircraft.Fins.MASS, prob, self.meta_data)
             if val != 0.0 or val != 'Not Found in Model':
-                f.write(f'|\tFins|{val}|{units}|\n')
+                f.write(f'|{tab}Fins|{val}|{units}|\n')
             f.write('||||\n')
 
             val, units = find_variable_in_problem(Aircraft.Fuselage.MASS, prob, self.meta_data)
@@ -180,17 +137,17 @@ class CoreMassBuilder(MassBuilder):
             val, units = find_variable_in_problem(
                 Aircraft.LandingGear.TOTAL_MASS, prob, self.meta_data
             )
-            f.write(f'|\tLanding Gear Group|{val}|{units}|\n')
+            f.write(f'|Landing Gear Group|{val}|{units}|\n')
             val, units = find_variable_in_problem(
                 Aircraft.LandingGear.MAIN_GEAR_MASS, prob, self.meta_data
             )
             if val != 0.0 or val != 'Not Found in Model':
-                f.write(f'|\tMain Gear|{val}|{units}|\n')
+                f.write(f'|{tab}Main Gear|{val}|{units}|\n')
             val, units = find_variable_in_problem(
                 Aircraft.LandingGear.NOSE_GEAR_MASS, prob, self.meta_data
             )
             if val != 0.0 or val != 'Not Found in Model':
-                f.write(f'|\tNose Gear|{val}|{units}|\n')
+                f.write(f'|{tab}Nose Gear|{val}|{units}|\n')
             f.write('||||\n')
 
             val, units = find_variable_in_problem(Aircraft.Nacelle.MASS, prob, self.meta_data)
@@ -202,7 +159,7 @@ class CoreMassBuilder(MassBuilder):
             val, units = find_variable_in_problem(
                 Aircraft.Design.STRUCTURE_MASS, prob, self.meta_data
             )
-            f.write(f'|Total Structure|{val}|{units}|\n')
+            f.write(f'|Structure Mass|{val}|{units}|\n')
             f.write('||||\n')
 
             val, units = find_variable_in_problem(Aircraft.Propulsion.MASS, prob, self.meta_data)
@@ -210,69 +167,73 @@ class CoreMassBuilder(MassBuilder):
             val, units = find_variable_in_problem(
                 Aircraft.Propulsion.TOTAL_ENGINE_MASS, prob, self.meta_data
             )
-            f.write(f'|\tEngines|{val}|{units}|\n')
+            f.write(f'|{tab}Engines|{val}|{units}|\n')
             val, units = find_variable_in_problem(Aircraft.Engine.MASS, prob, self.meta_data)
             for i, engine in enumerate(engine_models):
                 if isinstance(val, (np.ndarray, list, tuple)):
                     val = val[i]
-                f.write(f'|\t\t{engine.name}|{val} ({val * num_engines[i]} total)|{units}|\n')
+                f.write(f'|{tab}{tab}{engine.name}|{val} ({val * num_engines[i]} total)|{units}|\n')
+
+            val, units = find_variable_in_problem(
+                Aircraft.Propulsion.TOTAL_THRUST_REVERSERS_MASS, prob, self.meta_data
+            )
+            f.write(f'|{tab}Thrust Reversers|{val}|{units}|\n')
+
             val, units = find_variable_in_problem(
                 Aircraft.Propulsion.TOTAL_ENGINE_CONTROLS_MASS, prob, self.meta_data
             )
+            f.write(f'|{tab}Engine Controls|{val}|{units}|\n')
 
-            f.write(f'|\tEngine Controls|{val}|{units}|\n')
             val, units = find_variable_in_problem(
                 Aircraft.Propulsion.TOTAL_STARTER_MASS, prob, self.meta_data
             )
-            f.write(f'|\tStarting System|{val}|{units}|\n')
+            f.write(f'|{tab}Starting System|{val}|{units}|\n')
 
             val, units = find_variable_in_problem(Aircraft.Battery.MASS, prob, self.meta_data)
-            if val != 0.0 or val != 'Not Found in Model':
-                f.write(f'|\tBattery|{val}|{units}|\n')
+            if val != 0.0 and val != 'Not Found in Model':
+                f.write(f'|{tab}Battery|{val}|{units}|\n')
             f.write('|||\n')
-
-            val, units = find_variable_in_problem(Aircraft.Controls.MASS, prob, self.meta_data)
-            f.write(f'|Flight Controls|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(Aircraft.APU.MASS, prob, self.meta_data)
-            f.write(f'|Auxiliary Power|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(Aircraft.Instruments.MASS, prob, self.meta_data)
-            f.write(f'|Instruments|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(Aircraft.Hydraulics.MASS, prob, self.meta_data)
-            f.write(f'|Hydraulics|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(Aircraft.Electrical.MASS, prob, self.meta_data)
-            f.write(f'|Electrical|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(Aircraft.Avionics.MASS, prob, self.meta_data)
-            f.write(f'|Avionics|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(Aircraft.Furnishings.MASS, prob, self.meta_data)
-            f.write(f'|Furnishings|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(
-                Aircraft.AirConditioning.MASS, prob, self.meta_data
-            )
-            f.write(f'|Environmental Control|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(Aircraft.APU.MASS, prob, self.meta_data)
-            f.write(f'|Auxiliary Power|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(Aircraft.AntiIcing.MASS, prob, self.meta_data)
-            f.write(f'|Anti-Icing|{val}|{units}|\n')
-
-            val, units = find_variable_in_problem(
-                Aircraft.CrewPayload.CARGO_CONTAINER_MASS, prob, self.meta_data
-            )
-            f.write(f'|Load & Handling|{val}|{units}|\n')
-            f.write('||||\n')
 
             val, units = find_variable_in_problem(
                 Aircraft.Design.SYSTEMS_AND_EQUIPMENT_MASS, prob, self.meta_data
             )
-            f.write(f'|Total Systems and Equipment|{val}|{units}|\n')
+            f.write(f'|Systems and Equipment|{val}|{units}|\n')
+            val, units = find_variable_in_problem(Aircraft.Controls.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Flight Controls|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(Aircraft.APU.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Auxiliary Power|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(Aircraft.Instruments.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Instruments|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(Aircraft.Hydraulics.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Hydraulics|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(Aircraft.Electrical.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Electrical|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(Aircraft.Avionics.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Avionics|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(Aircraft.Furnishings.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Furnishings|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(
+                Aircraft.AirConditioning.MASS, prob, self.meta_data
+            )
+            f.write(f'|{tab}Environmental Control|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(Aircraft.APU.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Auxiliary Power|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(Aircraft.AntiIcing.MASS, prob, self.meta_data)
+            f.write(f'|{tab}Anti-Icing|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(
+                Aircraft.CrewPayload.CARGO_CONTAINER_MASS, prob, self.meta_data
+            )
+            f.write(f'|{tab}Load & Handling|{val}|{units}|\n')
             f.write('||||\n')
 
             val, units = find_variable_in_problem(
@@ -280,6 +241,7 @@ class CoreMassBuilder(MassBuilder):
             )
             if val != 0.0 or val != 'Not Found in Model':
                 f.write(f'|Empty Mass Margin|{val}|{units}|\n')
+            f.write('||||\n')
 
             val, units = find_variable_in_problem(Aircraft.Design.EMPTY_MASS, prob, self.meta_data)
             f.write(f'|Empty Mass|{val}|{units}|\n')
@@ -294,17 +256,23 @@ class CoreMassBuilder(MassBuilder):
             val2, units = find_variable_in_problem(
                 Aircraft.CrewPayload.FLIGHT_CREW_MASS, prob, self.meta_data
             )
-            f.write(f'|\tCrew|{val1 + val2}|{units}|\n')
+            f.write(f'|{tab}Crew|{val1 + val2}|{units}|\n')
+
+            val, units = find_variable_in_problem(
+                Aircraft.CrewPayload.PASSENGER_SERVICE_MASS, prob, self.meta_data
+            )
+            f.write(f'|{tab}Passenger Service|{val}|{units}|\n')
 
             val, units = find_variable_in_problem(
                 Aircraft.Fuel.UNUSABLE_FUEL_MASS, prob, self.meta_data
             )
-            f.write(f'|\tUnusable Fuel|{val}|{units}|\n')
+            f.write(f'|{tab}Unusable Fuel|{val}|{units}|\n')
 
             val, units = find_variable_in_problem(
                 Aircraft.Propulsion.TOTAL_ENGINE_OIL_MASS, prob, self.meta_data
             )
-            f.write(f'|\tOil|{val}|{units}|\n')
+            f.write(f'|{tab}Oil|{val}|{units}|\n')
+            f.write('||||\n')
 
             val, units = find_variable_in_problem(
                 Mission.Summary.OPERATING_MASS, prob, self.meta_data
@@ -313,14 +281,20 @@ class CoreMassBuilder(MassBuilder):
             f.write('||||\n')
 
             val, units = find_variable_in_problem(
+                Aircraft.CrewPayload.TOTAL_PAYLOAD_MASS, prob, self.meta_data
+            )
+            f.write(f'|Payload|{val}|{units}|\n')
+
+            val, units = find_variable_in_problem(
                 Aircraft.CrewPayload.PASSENGER_PAYLOAD_MASS, prob, self.meta_data
             )
-            f.write(f'|Passengers|{val}|{units}|\n')
+            f.write(f'|{tab}Passengers|{val}|{units}|\n')
 
             val, units = find_variable_in_problem(
                 Aircraft.CrewPayload.CARGO_MASS, prob, self.meta_data
             )
-            f.write(f'|Cargo|{val}|{units}|\n')
+            f.write(f'|{tab}Cargo|{val}|{units}|\n')
+            f.write('||||\n')
 
             val, units = find_variable_in_problem(
                 Mission.Summary.ZERO_FUEL_MASS, prob, self.meta_data
@@ -330,6 +304,7 @@ class CoreMassBuilder(MassBuilder):
 
             val, units = find_variable_in_problem(Mission.Summary.FUEL_MASS, prob, self.meta_data)
             f.write(f'|Fuel|{val}|{units}|\n')
+            f.write('||||\n')
 
             val, units = find_variable_in_problem(Mission.Design.GROSS_MASS, prob, self.meta_data)
             f.write(f'|Gross Mass|{val}|{units}|\n')
