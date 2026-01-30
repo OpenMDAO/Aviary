a1d2cdc208d0a8751b7834b365c89341a8aad109
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2026-01-30 11:08:06 -0500

    Added more info about interp method to the metadata

--- aviary/variable_info/variable_meta_data.py
+++ aviary/variable_info/variable_meta_data.py
@@ -2167,7 +2167,7 @@ add_meta_data(
     types=str,
     desc="method used for interpolation on an engine deck's data file, allowable values are "
     'table methods from openmdao.components.interp_util.interp. Engine models only use the '
-    'models avilable to the MetamodelUnstructured component. These are listed here: '
+    'methods avilable to the MetaModelSemiStructuredComp component. These are listed here: '
     'https://openmdao.org/newdocs/versions/latest/features/building_blocks/components/'
     'metamodelsemistructured_comp.html',
     multivalue=True,
