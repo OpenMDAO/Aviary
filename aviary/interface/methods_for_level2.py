e803f6c70fecde35e7cfab6e723ec9a3570bb885
Owen Shepherd <owsh8290@colorado.edu>
2026-02-05 07:41:08 -0800

    Creates logic to pass test_report_directory_naming
    This adds some logic to update the naming of
    the output directories in line with the
    aforementioned unit test.

--- aviary/interface/methods_for_level2.py
+++ aviary/interface/methods_for_level2.py
@@ -1366,7 +1366,14 @@ class AviaryProblem(om.Problem):
             )
 
         if name is None:
-            name = name = self._name + '_off_design'
+            suggestion = self._name + "_" + str(problem_type.value)
+            # For while loop / numbering names
+            suggestion_update = suggestion
+            counter = 2
+            while os.path.exists(os.path.join(".", suggestion_update + "_out")):
+                suggestion_update = suggestion + "_" + str(counter)
+                counter += 1
+            name = suggestion_update
         off_design_prob = AviaryProblem(name=name)
 
         # Set up problem for mission, such as equations of motion, configurators, etc.
