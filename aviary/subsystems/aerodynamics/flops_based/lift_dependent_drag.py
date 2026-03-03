99bf36826041df12fcc4901ea728f147db04c027
Xun Jiang <xun.j.jiang@nasa.gov>
2026-03-02 20:13:24 -0500

    update edge_interp() due to NumPy update

--- aviary/subsystems/aerodynamics/flops_based/lift_dependent_drag.py
+++ aviary/subsystems/aerodynamics/flops_based/lift_dependent_drag.py
@@ -82,8 +82,7 @@ class LiftDependentDrag(om.ExplicitComponent):
             )
         )
 
-        # TODO: comparing to the return types of inner_interp(), this seems to be wrong
-        return FCDP, dFCDP_dDEL[:, 0], dFCDP_dDEL[:, 1], dFCDP_dA
+        return FCDP[0], dFCDP_dDEL[:, 0][0], dFCDP_dDEL[:, 1][0], dFCDP_dA[0]
 
     def inner_interp(
         self, arrA, FCDP1, FCDP2, FCDP3, FCDP4, FCDP5, dFCDP1, dFCDP2, dFCDP3, dFCDP4, dFCDP5, A
