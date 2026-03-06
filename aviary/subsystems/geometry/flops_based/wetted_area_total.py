e026e911fd603225d33f56b545ec0ca23efe48c5
Xun Jiang <xjjiang@users.noreply.github.com>
2026-03-05 16:34:57 -0800

    Refactor input addition for clarity in wetted_area_total.py

--- aviary/subsystems/geometry/flops_based/wetted_area_total.py
+++ aviary/subsystems/geometry/flops_based/wetted_area_total.py
@@ -546,8 +546,12 @@ class BWBWing_SWet(om.ExplicitComponent):
         add_aviary_input(self, Aircraft.Fuselage.MAX_WIDTH, units='ft')
         add_aviary_input(self, Aircraft.Wing.GLOVE_AND_BAT, units='ft**2')
         add_aviary_input(self, Aircraft.Wing.SPAN, units='ft')
-        self.add_input('BWB_CHORD_PER_SEMISPAN_DISTRIBUTION', shape=num_inp_stations, units='unitless')
-        self.add_input('BWB_THICKNESS_TO_CHORD_DISTRIBUTION', shape=num_inp_stations, units='unitless')
+        self.add_input(
+            'BWB_CHORD_PER_SEMISPAN_DISTRIBUTION', shape=num_inp_stations, units='unitless'
+        )
+        self.add_input(
+            'BWB_THICKNESS_TO_CHORD_DISTRIBUTION', shape=num_inp_stations, units='unitless'
+        )
 
         add_aviary_output(self, Aircraft.Wing.WETTED_AREA, units='ft**2')
 
