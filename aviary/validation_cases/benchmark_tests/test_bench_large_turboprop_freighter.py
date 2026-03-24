2d240432ce3d15597ccc3a1ca89d6a36a2a08ff4
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2026-03-24 09:04:05 -0400

    Lower drag for the turboprop problem

--- aviary/validation_cases/benchmark_tests/test_bench_large_turboprop_freighter.py
+++ aviary/validation_cases/benchmark_tests/test_bench_large_turboprop_freighter.py
@@ -52,7 +52,7 @@ class LargeTurbopropFreighterBenchmark(unittest.TestCase):
         prob.check_and_preprocess_inputs()
 
         prob.build_model()
-        prob.add_driver('IPOPT', max_iter=0, verbosity=0)
+        prob.add_driver('IPOPT', max_iter=100, verbosity=0)
         prob.add_design_variables()
         prob.add_objective()
         prob.setup()
