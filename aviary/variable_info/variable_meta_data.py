760f6b6bab7df9b457db2c2ab9af360a35557833
Bennett <cmbenne3@ndc.nasa.gov>
2026-03-25 11:55:43 -0400

    fix alphabetization of variable metadata

--- aviary/variable_info/variable_meta_data.py
+++ aviary/variable_info/variable_meta_data.py
@@ -7139,6 +7139,23 @@ add_meta_data(
 #  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'
 #  ============================================================================================================================================
 
+add_meta_data(
+    Mission.FINAL_MASS,
+    meta_data=_MetaData,
+    historical_name={'GASP': 'None', 'FLOPS': None, 'LEAPS1': None},  # TODO: Check on these
+    units='lbm',
+    desc='The final weight of the vehicle at the end of the last regular_phase (does not include reserve phases).',
+)
+
+add_meta_data(
+    Mission.FINAL_TIME,
+    meta_data=_MetaData,
+    historical_name={'GASP': 'None', 'FLOPS': None, 'LEAPS1': None},  # TODO: Check on these
+    units='min',
+    desc='Total mission time from the start of the first regular_phase'
+    'to the end of the last regular_phase (does not include reserve phases).',
+)
+
 #   _____                         _                    _           _
 #  / ____|                       | |                  (_)         | |
 # | |        ___    _ __    ___  | |_   _ __    __ _   _   _ __   | |_   ___
@@ -7713,23 +7730,6 @@ add_meta_data(
     units='lbm',
     desc='mass of the aircraft at the end of cruise',
     default_value=0.0,
-)
-
-add_meta_data(
-    Mission.FINAL_MASS,
-    meta_data=_MetaData,
-    historical_name={'GASP': 'None', 'FLOPS': None, 'LEAPS1': None},  # TODO: Check on these
-    units='lbm',
-    desc='The final weight of the vehicle at the end of the last regular_phase (does not include reserve phases).',
-)
-
-add_meta_data(
-    Mission.FINAL_TIME,
-    meta_data=_MetaData,
-    historical_name={'GASP': 'None', 'FLOPS': None, 'LEAPS1': None},  # TODO: Check on these
-    units='min',
-    desc='Total mission time from the start of the first regular_phase'
-    'to the end of the last regular_phase (does not include reserve phases).',
 )
 
 add_meta_data(
