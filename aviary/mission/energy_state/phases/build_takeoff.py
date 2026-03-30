b561c4f0abba45d4be85e6ee841c997da52df633
Eliot Aretskin-Hariton <eliot.d.aretskin-hariton@nasa.gov>
2026-03-30 14:42:03 +0000

    added defalt val for GROSS_MASS for a test and loosened tolerance slightly.

--- aviary/mission/energy_state/phases/build_takeoff.py
+++ aviary/mission/energy_state/phases/build_takeoff.py
@@ -25,7 +25,7 @@ class Takeoff:
     def __init__(
         self,
         airport_altitude=None,
-        ramp_mass=None,
+        ramp_mass=None, # why is this even an option?
         num_engines=None,
     ):
         self.airport_altitude = airport_altitude  # ft
