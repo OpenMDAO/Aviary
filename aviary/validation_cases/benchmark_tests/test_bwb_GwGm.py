5b21896271ad2f27a43e8ccd28106592dbd4f824
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2026-04-01 12:39:13 -0400

    Add missing import

--- aviary/validation_cases/benchmark_tests/test_bwb_GwGm.py
+++ aviary/validation_cases/benchmark_tests/test_bwb_GwGm.py
@@ -8,7 +8,7 @@ from openmdao.utils.testing_utils import require_pyoptsparse, use_tempdirs
 from aviary.models.aircraft.blended_wing_body.generic_BWB_2dof_phase_info import phase_info
 
 from aviary.interface.run_aviary import run_aviary
-from aviary.variable_info.variables import Mission
+from aviary.variable_info.variables import Aircraft, Mission
 
 
 @use_tempdirs
