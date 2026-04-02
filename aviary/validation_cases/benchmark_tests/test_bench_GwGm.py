8ec3e88323552e7809ff84a115543ec181533a4a
Xun Jiang <xun.j.jiang@nasa.gov>
2026-04-02 17:16:27 -0400

    replace Mission.Design.GROSS_MASS by Mission.GROSS_MASS

--- aviary/validation_cases/benchmark_tests/test_bench_GwGm.py
+++ aviary/validation_cases/benchmark_tests/test_bench_GwGm.py
@@ -29,9 +29,9 @@ class ProblemPhaseTestCase(unittest.TestCase):
 
         # There are no truth values for these.
         expected_values = {
-            (Mission.Design.GROSS_MASS, 'lbm'): 171414.17171104,
-            (Mission.Summary.OPERATING_MASS, 'lbm'): 94986.583699,
-            (Mission.Summary.TOTAL_FUEL_MASS, 'lbm'): 40451.68735078,
+            (Mission.GROSS_MASS, 'lbm'): 171414.17171104,
+            (Mission.OPERATING_MASS, 'lbm'): 94986.583699,
+            (Mission.TOTAL_FUEL, 'lbm'): 40451.68735078,
             (Mission.Landing.GROUND_DISTANCE, 'ft'): 2657.88663983,
             (Mission.RANGE, 'NM'): 3675.0,
             (Mission.FINAL_MASS, 'lbm'): 136087.98897716,
