6f8cf485ae8bb668e84597be27ce0663dd801fb2
Xun Jiang <xun.j.jiang@nasa.gov>
2026-04-21 12:23:58 -0400

    combined drag factors and technology factorsin fortran_to_aviary

--- aviary/utils/fortran_to_aviary.py
+++ aviary/utils/fortran_to_aviary.py
@@ -800,6 +800,80 @@ def update_gasp_options(vehicle_data, verbosity=Verbosity.BRIEF):
         except:
             pass
 
+    # Friction factors (combine drag factor and technology factor into one factor)
+    round_position = 6
+    if Aircraft.Design.Fuselage_DRAG_FACTOR in input_values:
+        drag_scaler = input_values.get_val(Aircraft.Design.Fuselage_DRAG_FACTOR, 'unitless')[0]
+        try:
+            tech_factor = unused_values.get_item('INGASP.FCFFT')[0][0]
+            unused_values.delete('INGASP.FCFFT')
+        except:
+            tech_factor = 1.0
+        drag_scaler = round(drag_scaler * tech_factor, round_position)
+        input_values.set_val(Aircraft.Design.Fuselage_DRAG_FACTOR, [drag_scaler], 'unitless')
+
+    if Aircraft.Design.HorizontalTail_DRAG_FACTOR in input_values:
+        drag_scaler = input_values.get_val(Aircraft.Design.HorizontalTail_DRAG_FACTOR, 'unitless')[
+            0
+        ]
+        try:
+            tech_factor = unused_values.get_item('INGASP.FCFHTT')[0][0]
+            unused_values.delete('INGASP.FCFHTT')
+        except:
+            tech_factor = 1.0
+        drag_scaler = round(drag_scaler * tech_factor, round_position)
+        input_values.set_val(Aircraft.Design.HorizontalTail_DRAG_FACTOR, [drag_scaler], 'unitless')
+
+    if Aircraft.Design.INTERFERENCE_DRAG_FACTOR in input_values:
+        drag_scaler = input_values.get_val(Aircraft.Design.INTERFERENCE_DRAG_FACTOR, 'unitless')[0]
+        try:
+            tech_factor = unused_values.get_item('INGASP.FCKIT')[0][0]
+            unused_values.delete('INGASP.FCKIT')
+        except:
+            tech_factor = 1.0
+        drag_scaler = round(drag_scaler * tech_factor, round_position)
+        input_values.set_val(Aircraft.Design.INTERFERENCE_DRAG_FACTOR, [drag_scaler], 'unitless')
+
+    if Aircraft.Design.Nacelle_DRAG_FACTOR in input_values:
+        drag_scaler = input_values.get_val(Aircraft.Design.Nacelle_DRAG_FACTOR, 'unitless')[0]
+        try:
+            tech_factor = unused_values.get_item('INGASP.FCFNT')[0][0]
+            unused_values.delete('INGASP.FCFNT')
+        except:
+            tech_factor = 1.0
+        drag_scaler = round(drag_scaler * tech_factor, round_position)
+        input_values.set_val(Aircraft.Design.Nacelle_DRAG_FACTOR, [drag_scaler], 'unitless')
+
+    if Aircraft.Design.STRUT_DRAG_FACTOR in input_values:
+        drag_scaler = input_values.get_val(Aircraft.Design.STRUT_DRAG_FACTOR, 'unitless')[0]
+        try:
+            tech_factor = unused_values.get_item('INGASP.FCFVTT')[0][0]
+            unused_values.delete('INGASP.FCFVTT')
+        except:
+            tech_factor = 1.0
+        drag_scaler = round(drag_scaler * tech_factor, round_position)
+        input_values.set_val(Aircraft.Design.STRUT_DRAG_FACTOR, [drag_scaler], 'unitless')
+
+    if Aircraft.Design.VerticalTail_DRAG_FACTOR in input_values:
+        drag_scaler = input_values.get_val(Aircraft.Design.VerticalTail_DRAG_FACTOR, 'unitless')[0]
+        try:
+            tech_factor = unused_values.get_item('INGASP.FCFVTT')[0][0]
+            unused_values.delete('INGASP.FCFVTT')
+        except:
+            tech_factor = 1.0
+        drag_scaler = round(drag_scaler * tech_factor, round_position)
+        input_values.set_val(Aircraft.Design.VerticalTail_DRAG_FACTOR, [drag_scaler], 'unitless')
+
+    if Aircraft.Design.Wing_DRAG_FACTOR in input_values:
+        drag_scaler = input_values.get_val(Aircraft.Design.Wing_DRAG_FACTOR, 'unitless')[0]
+        try:
+            tech_factor = unused_values.get_item('INGASP.FCFWT')[0][0]
+            unused_values.delete('INGASP.FCFWT')
+        except:
+            tech_factor = 1.0
+        drag_scaler = round(drag_scaler * tech_factor, round_position)
+        input_values.set_val(Aircraft.Design.Wing_DRAG_FACTOR, [drag_scaler], 'unitless')
+
     # Variables required by GASP, but no default values are provided in GASP
     missing_vars = []
     if Aircraft.Wing.ZERO_LIFT_ANGLE not in input_values:
