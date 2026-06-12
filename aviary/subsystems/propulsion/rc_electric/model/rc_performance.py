92983b492ad3b750bc7a5cef1b657fb52fe52c42
Daley <tadaley@ndc.nasa.gov>
2026-06-12 15:41:44 -0400

    Took Out openmdao.jax, not used

--- aviary/subsystems/propulsion/rc_electric/model/rc_performance.py
+++ aviary/subsystems/propulsion/rc_electric/model/rc_performance.py
@@ -1,6 +1,5 @@
 import numpy as np
 import openmdao.api as om
-import openmdao.jax as omj
 from aviary.variable_info.functions import add_aviary_input, add_aviary_option, add_aviary_output
 from aviary.variable_info.variables import Aircraft, Dynamic, Settings
 
