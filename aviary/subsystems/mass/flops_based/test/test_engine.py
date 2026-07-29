645c48cdfa2f40916783a60a3a255f742987cb61
Xun Jiang <xun.j.jiang@nasa.gov>
2026-07-29 13:52:38 -0400

    combined bwb cases into EngineMassTest

--- aviary/subsystems/mass/flops_based/test/test_engine.py
+++ aviary/subsystems/mass/flops_based/test/test_engine.py
@@ -13,7 +13,6 @@ from aviary.utils.functions import get_path
 from aviary.utils.preprocessors import preprocess_propulsion
 from aviary.utils.test_utils.variable_test import assert_match_varnames
 from aviary.validation_cases.validation_tests import (
-    Version,
     flops_validation_test,
     get_flops_case_names,
     get_flops_options,
@@ -21,15 +20,13 @@ from aviary.validation_cases.validation_tests import (
 )
 from aviary.variable_info.variables import Aircraft, Settings
 
-bwb_cases = ['BWBsimpleFLOPS', 'BWBdetailedFLOPS', 'BWB300FLOPS']
-
 
 @use_tempdirs
 class EngineMassTest(unittest.TestCase):
     def setUp(self):
         self.prob = om.Problem()
 
-    @parameterized.expand(get_flops_case_names(omit=bwb_cases), name_func=print_case)
+    @parameterized.expand(get_flops_case_names(), name_func=print_case)
     def test_case(self, case_name):
         prob = self.prob
 
@@ -212,55 +209,6 @@ class EngineMassTest(unittest.TestCase):
 
     def test_IO(self):
         assert_match_varnames(self.prob.model)
-
-
-@use_tempdirs
-class BWBEngineMassTest(unittest.TestCase):
-    """Tests engine mass calculation for BWB."""
-
-    def setUp(self):
-        self.prob = om.Problem()
-
-    # @parameterized.expand(get_flops_case_names(only=bwb_cases), name_func=print_case)
-    def test_case(
-        self,
-    ):
-        case_name = 'BWBsimpleFLOPS'
-        prob = self.prob
-
-        prob.model.add_subsystem(
-            'engine_mass',
-            EngineMass(),
-            promotes_inputs=['*'],
-            promotes_outputs=['*'],
-        )
-
-        prob.model_options['*'] = get_flops_options(case_name, preprocess=True)
-
-        prob.setup(check=False, force_alloc_complex=True)
-        prob.set_val(Aircraft.Engine.MASS_SCALER, val=np.ones(1))
-
-        flops_validation_test(
-            self,
-            prob,
-            case_name,
-            input_keys=[
-                Aircraft.Engine.REFERENCE_MASS,
-                Aircraft.Engine.SCALED_SLS_THRUST,
-                Aircraft.Engine.REFERENCE_SLS_THRUST,
-                Aircraft.Engine.MASS_SCALER,
-            ],
-            output_keys=[
-                Aircraft.Engine.MASS,
-                Aircraft.Engine.ADDITIONAL_MASS,
-                Aircraft.Propulsion.TOTAL_ENGINE_MASS,
-            ],
-            list_inputs=True,
-            list_outputs=True,
-            version=Version.BWB,
-            rtol=1e-10,
-            atol=1e-10,
-        )
 
 
 if __name__ == '__main__':
