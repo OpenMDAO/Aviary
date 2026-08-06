d4963c7d080aa70af604c13ddca3e49d8b0b642d
Bennett <cmbenne3@ndc.nasa.gov>
2026-08-06 18:20:31 -0400

    aligning detailed landing with solved_2dof takeoff

--- aviary/validation_cases/benchmark_tests/test_bench_solved2dof.py
+++ aviary/validation_cases/benchmark_tests/test_bench_solved2dof.py
@@ -2,6 +2,7 @@ import unittest
 import aviary.api as av
 
 from aviary.models.missions.solved2dof_default import phase_info
+from aviary.models.missions.solved2dof_landing_default import phase_info as phase_info_landing
 from openmdao.utils.assert_utils import assert_near_equal
 from openmdao.utils.testing_utils import use_tempdirs
 
@@ -23,6 +24,20 @@ class TestBenchSolved2DOF(unittest.TestCase):
         tol = 1e-4
         assert_near_equal(prob.get_val(av.Mission.FINAL_TIME, units='s'), 108.84030411, tol)
         assert_near_equal(prob.get_val(av.Mission.FUEL_MASS, units='lbm'), 459.3830223, tol)
+
+    def test_bench_Solved2DOF_landing(self):
+        prob = av.run_aviary(
+            aircraft_data='validation_cases/validation_data/test_models/aircraft_for_bench_solved2dof.csv',
+            phase_info=phase_info_landing,
+            optimizer='IPOPT',
+            objective_type='time',
+            max_iter=100,
+        )
+
+        self.assertTrue(prob.result.success)
+        tol = 1e-4
+        assert_near_equal(prob.get_val(av.Mission.FINAL_TIME, units='s'), 68.30257602, tol)
+        assert_near_equal(prob.get_val(av.Mission.FUEL_MASS, units='lbm'), 98.90626609, tol)
 
     # def test_bench_Solved2DOF_fuel_burned(self):
     #     prob = av.run_aviary(
