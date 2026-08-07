f4b50a9c0247a1c73c56f95a43ea4a4f0c37c07d
Bennett <cmbenne3@ndc.nasa.gov>
2026-08-07 09:35:12 -0400

    update solved_2dof landing test to allow specification of a reference for time objective to allow better convergence

--- aviary/validation_cases/benchmark_tests/test_bench_solved2dof.py
+++ aviary/validation_cases/benchmark_tests/test_bench_solved2dof.py
@@ -26,18 +26,24 @@ class TestBenchSolved2DOF(unittest.TestCase):
         assert_near_equal(prob.get_val(av.Mission.FUEL_MASS, units='lbm'), 459.3830223, tol)
 
     def test_bench_Solved2DOF_landing(self):
-        prob = av.run_aviary(
-            aircraft_data='validation_cases/validation_data/test_models/aircraft_for_bench_solved2dof.csv',
-            phase_info=phase_info_landing,
-            optimizer='IPOPT',
-            objective_type='time',
-            max_iter=100,
+        # This problem solves better with a reduced ref for objective time, therefore need to call add_objectve()
+        prob = av.AviaryProblem()
+        prob.load_inputs(
+            'validation_cases/validation_data/test_models/aircraft_for_bench_solved2dof.csv',
+            phase_info_landing,
         )
+        prob.check_and_preprocess_inputs()
+        prob.build_model()
+        prob.add_driver('IPOPT', max_iter=100)
+        prob.add_design_variables()
+        prob.add_objective('time', ref=1e2)
+        prob.setup()
+        prob.run_aviary_problem()
 
         self.assertTrue(prob.result.success)
-        tol = 1e-4
-        assert_near_equal(prob.get_val(av.Mission.FINAL_TIME, units='s'), 68.30257602, tol)
-        assert_near_equal(prob.get_val(av.Mission.FUEL_MASS, units='lbm'), 98.90626609, tol)
+        tol = 1e-6
+        assert_near_equal(prob.get_val(av.Mission.FINAL_TIME, units='s'), 68.30353617, tol)
+        assert_near_equal(prob.get_val(av.Mission.FUEL_MASS, units='lbm'), 98.91566618, tol)
 
 
 if __name__ == '__main__':
