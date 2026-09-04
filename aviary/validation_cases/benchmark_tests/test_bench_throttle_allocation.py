6ac78b088e1b933a5dbd47264361a74f722c870d
jkirk5 <jason.kirk@nasa.gov>
2026-09-04 17:55:44 -0400

    fixed high linear scaling factor in models & tests

--- aviary/validation_cases/benchmark_tests/test_bench_throttle_allocation.py
+++ aviary/validation_cases/benchmark_tests/test_bench_throttle_allocation.py
@@ -131,9 +131,9 @@ class ThrottleAllocationTestcase(unittest.TestCase):
         alloc_descent = prob.get_val('traj.descent.parameter_vals:throttle_allocations')
 
         with self.subTest('climb_allocation'):
-            assert_near_equal(alloc_climb[0], 0.5, tolerance=1e-2)
+            assert_near_equal(alloc_climb[0], 0.48777445, tolerance=1e-2)
         with self.subTest('cruise_allocation'):
-            assert_near_equal(alloc_cruise[0], 0.64523, tolerance=1e-2)
+            assert_near_equal(alloc_cruise[0], 0.34035731, tolerance=1e-2)
 
     @require_pyoptsparse(optimizer='SNOPT')
     def test_multiengine_dynamic(self):
@@ -172,11 +172,11 @@ class ThrottleAllocationTestcase(unittest.TestCase):
 
         with self.subTest('cruise_allocation'):
             # Cruise is pretty constant, check exact value.
-            assert_near_equal(alloc_cruise[0], 0.6452, tolerance=1e-2)
+            assert_near_equal(alloc_cruise[0], 0.33626162, tolerance=1e-2)
 
         with self.subTest('climb_allocation'):
-            # Check general trend: favors engine 1.
-            self.assertGreater(alloc_climb[2], 0.55)
+            # Check general trend: favors engine 2.
+            self.assertGreater(alloc_climb[2], 0.21)
 
 
 if __name__ == '__main__':
