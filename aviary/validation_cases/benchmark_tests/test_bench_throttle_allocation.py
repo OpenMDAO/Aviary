3d534c47f3aafe0af4c056262928e2d4ecadefba
jkirk5 <jason.kirk@nasa.gov>
2026-09-10 08:57:51 -0400

    reactivate using tempdirs for test

--- aviary/validation_cases/benchmark_tests/test_bench_throttle_allocation.py
+++ aviary/validation_cases/benchmark_tests/test_bench_throttle_allocation.py
@@ -44,7 +44,7 @@ inputs.set_val(Aircraft.Nacelle.LAMINAR_FLOW_LOWER, np.zeros(2))
 inputs.set_val(Aircraft.Nacelle.LAMINAR_FLOW_UPPER, np.zeros(2))
 
 
-# @use_tempdirs
+@use_tempdirs
 class ThrottleAllocationTestcase(unittest.TestCase):
     """Test the different throttle allocation methods for models with multiple, unique EngineModels."""
 
